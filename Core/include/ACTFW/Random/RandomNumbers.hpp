d6a49f94b8a08daa16a45d4e707c035a58534296
msmk <msmk@cern.ch>
2016-08-16 17:47:33 +0200

    Random/RandomNumbers: split Logger from Config and remove unused name

--- Core/include/ACTFW/Random/RandomNumbers.hpp
+++ Core/include/ACTFW/Random/RandomNumbers.hpp
@@ -19,7 +19,7 @@
 
 namespace FW {
     
-    ///  @enum class Distribution 
+    ///  @enum class Distribution
     enum class Distribution { uniform, gauss, landau, gamma };
     
     
@@ -37,48 +37,43 @@ namespace FW {
     public:
         
         /// @class Config
-        /// Nested Configuration class 
-        class Config {
-        public:
-            std::shared_ptr<Acts::Logger>   logger;             ///< the default logger 
-            unsigned int                    seed;               ///< random seed
-            std::array<double, 2>           uniform_parameters; ///< configuration uniform
-            std::array<double, 2>           gauss_parameters;   ///< configuration gauss
-            std::array<double, 2>           landau_parameters;  ///< configuration landau
-            std::array<double, 2>           gamma_parameters;   ///< configuration gamma 
-            std::string                     name;               ///< name identifier
-            
-            Config(const std::string& lname = "Algorithm",
-                   Acts::Logging::Level lvl = Acts::Logging::INFO)
-             : logger(Acts::getDefaultLogger(lname,lvl))
-             , seed(1234567890)
-             , name(lname)
-            {}
+        /// Nested Configuration class
+        struct Config {
+          /// random seed
+          unsigned int seed = 1234567890;
+          /// configuration uniform
+          std::array<double, 2> uniform_parameters = {0, 1};
+          /// configuration gauss
+          std::array<double, 2> gauss_parameters = {0, 1};
+          /// configuration landau
+          std::array<double, 2> landau_parameters = {0, 1};
+          /// configuration gamma
+          std::array<double, 2> gamma_parameters = {0, 1};
         };
         
-        /// Constructor 
-        RandomNumbers(const Config& cfg);
+        /// Constructor
+        RandomNumbers(const Config& cfg,
+                      std::unique_ptr<Acts::Logger> logger
+                      = Acts::getDefaultLogger("RandomNumbers", Acts::Logging::INFO));
     
-        /// Destructor 
+        /// Destructor
         ~RandomNumbers(){}
         
-        /// Draw the random number 
+        /// Draw the random number
         ///
         /// @param dPar is the indicator which distrubtion to be drawn
         double draw(Distribution dPar);
         
-        /// Ask for the seed 
+        /// Ask for the seed
         unsigned int seed() const { return m_cfg.seed; }
         
-        /// Draw random numbers 
-        const std::string& name() const { return m_cfg.name; }
-        
-        /// Set the configuration 
+        /// Set the configuration
         ProcessCode setConfiguration(const Config& sfg);
         
     private:
         
         Config           m_cfg;        ///< the configuration class
+        std::unique_ptr<Acts::Logger> m_logger;
         RandomEngine     m_engine;     ///< the random engine
         GaussDist        m_gauss;      ///< gauss distribution
         UniformDist      m_uniform;    ///< uniform distribution
@@ -88,8 +83,8 @@ namespace FW {
         const Acts::Logger&
         logger() const
         {
-          return *m_cfg.logger;
-        }   
+          return *m_logger;
+        }
         
         
     };
