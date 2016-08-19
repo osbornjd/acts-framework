a057af148211be609c7d5fa1d9df17d15062034d
jhrdinka <julia.hrdinka@cern.ch>
2016-08-19 11:52:03 +0200

    add possibility to hand over logging level and magneticfield

--- Examples/ExtrapolationTest/include/ACTFW/ExtrapolationTest/ExtrapolationUtils.hpp
+++ Examples/ExtrapolationTest/include/ACTFW/ExtrapolationTest/ExtrapolationUtils.hpp
@@ -6,10 +6,12 @@
 #define ACTFW_EXTRAPOLATIONUTILS
 
 #include <memory>
+#include "ACTS/Utilities/Logger.hpp"
 
 namespace Acts {
     class IExtrapolationEngine;
     class TrackingGeometry;
+    class IMagneticFieldSvc;
 }
 
 namespace FWE {
@@ -18,7 +20,7 @@ namespace FWE {
 /// @param geo shared_ptr to the Acts::TrackingGeometry which should be used for the extrapolation
 /// @return a fully initialized Acts::ExtrapoltionEngine
 std::shared_ptr<Acts::IExtrapolationEngine>
-    initExtrapolator(const std::shared_ptr<const Acts::TrackingGeometry>& geo);
+    initExtrapolator(const std::shared_ptr<const Acts::TrackingGeometry>& geo, std::shared_ptr<Acts::IMagneticFieldSvc> magFieldSvc, Acts::Logging::Level eLogLevel);
     
 }
 
--- Examples/ExtrapolationTest/src/ExtrapolationUtils.cpp
+++ Examples/ExtrapolationTest/src/ExtrapolationUtils.cpp
@@ -1,9 +1,50 @@
-//
-//  ExtrapolationUtils.cpp
-//  
-//
-//  Created by Julia Hrdinka on 19/08/16.
-//
-//
+///////////////////////////////////////////////////////////////////
+// ExtrapolationUtils.cpp
+///////////////////////////////////////////////////////////////////
 
-#include <stdio.h>
+#include "ACTFW/ExtrapolationTest/ExtrapolationUtils.hpp"
+#include "ACTS/Extrapolation/IExtrapolationEngine.hpp"
+#include "ACTS/Detector/TrackingGeometry.hpp"
+#include "ACTS/MagneticField/IMagneticFieldSvc.hpp"
+#include "ACTS/Extrapolation/RungeKuttaEngine.hpp"
+#include "ACTS/Extrapolation/MaterialEffectsEngine.hpp"
+#include "ACTS/Extrapolation/StaticNavigationEngine.hpp"
+#include "ACTS/Extrapolation/StaticEngine.hpp"
+#include "ACTS/Extrapolation/ExtrapolationEngine.hpp"
+
+
+std::shared_ptr<Acts::IExtrapolationEngine>
+FWE::initExtrapolator(const std::shared_ptr<const Acts::TrackingGeometry>& geo, std::shared_ptr<Acts::IMagneticFieldSvc> magFieldSvc, Acts::Logging::Level eLogLevel)
+{
+    // EXTRAPOLATOR - set up the extrapolator
+    // (a) RungeKuttaPropagtator
+    auto propConfig         = Acts::RungeKuttaEngine::Config();
+    propConfig.fieldService = magFieldSvc;
+    auto propEngine         = std::make_shared<Acts::RungeKuttaEngine>(propConfig);
+    // (b) MaterialEffectsEngine
+    auto matConfig      = Acts::MaterialEffectsEngine::Config();
+    auto materialEngine = std::make_shared<Acts::MaterialEffectsEngine>(matConfig);
+     // (c) StaticNavigationEngine
+    auto navConfig                  = Acts::StaticNavigationEngine::Config();
+    navConfig.propagationEngine     = propEngine;
+    navConfig.materialEffectsEngine = materialEngine;
+    navConfig.trackingGeometry      = geo;
+    auto navEngine = std::make_shared<Acts::StaticNavigationEngine>(navConfig);
+    // (d) the StaticEngine
+    auto statConfig                  = Acts::StaticEngine::Config();
+    statConfig.propagationEngine     = propEngine;
+    statConfig.navigationEngine      = navEngine;
+    statConfig.materialEffectsEngine = materialEngine;
+    auto statEngine                  = std::make_shared<Acts::StaticEngine>(statConfig);
+    // (e) the material engine
+    auto exEngineConfig                 = Acts::ExtrapolationEngine::Config();
+    exEngineConfig.trackingGeometry     = geo;
+    exEngineConfig.propagationEngine    = propEngine;
+    exEngineConfig.navigationEngine     = navEngine;
+    exEngineConfig.extrapolationEngines = {statEngine};
+    auto exEngine = std::make_shared<Acts::ExtrapolationEngine>(exEngineConfig);
+    exEngine->setLogger(
+                        Acts::getDefaultLogger("ExtrapolationEngine", eLogLevel));
+    
+    return exEngine;
+}
