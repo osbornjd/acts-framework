1387e2eb3cc09f675bfb3feaa1da6d5b5df7371a
Hadrien Grasland <grasland@lal.in2p3.fr>
2016-10-25 15:46:47 +0200

    Built an abstraction around OpenMP's parallel for loop
    * This allows us to exit the loop early in compilers that do not support OpenMP 4, such as clang.
    * It can also eases switching to non-OpenMP tasking backends, should we decide to do so in the future.

---
+++ Core/include/ACTFW/Concurrency/parallel_for.hpp
@@ -0,0 +1,83 @@
+//
+//  parallel_for.h
+//  ACTFW
+//
+//  Created by Hadrien Grasland on 11/05/16.
+//
+
+/// @file
+
+#ifndef ACTFW_CONCURRENCY_PARALLEL_FOR_H
+#define ACTFW_CONCURRENCY_PARALLEL_FOR_H 1
+
+#include "ACTFW/Framework/ProcessCode.hpp"
+
+/// We will use this simple helper around the _Pragma operator of C99
+/// and C++11, if it is not defined elsewhere
+///
+#ifndef ACTFW_PRAGMA
+#define ACTFW_PRAGMA(x) _Pragma(#x)
+#endif
+
+/// Out-of-order multithreaded equivalent of the following serial loop
+/// (macro arguments capitalized for emphasis):
+///
+///   for(size_t INDEX = START; index < END; ++index) {
+///      ...
+///   }
+///
+/// One difference with a serial loop is that you cannot easily abort
+/// the computation using break and return statements. This is a
+/// limitation of the OpenMP 3.1 tasking backend that we currently use.
+///
+/// Replacements for the common use cases of break and return are
+/// provided in ACTFW_PARALLEL_FOR_BREAK and ACTFW_PARALLEL_FOR_ABORT.
+///
+/// Another difference with a serial loop is that the code which is
+/// passed as an argument should contain no preprocessor directive.
+///
+/// @param index must be unique within the enclosing scope.
+///
+#define ACTFW_PARALLEL_FOR(index, start, end, ...)                   \
+   {                                                                 \
+      bool actfw_parallel_break_##index = false;                     \
+      bool actfw_parallel_abort_##index = false;                     \
+                                                                     \
+      ACTFW_PRAGMA(omp parallel for)                                 \
+      for(size_t index = (start); index < (end); ++index)            \
+      {                                                              \
+         ACTFW_PRAGMA(omp flush (actfw_parallel_break_##index))      \
+         if(actfw_parallel_break_##index) continue;                  \
+         __VA_ARGS__                                                 \
+      }                                                              \
+                                                                     \
+      if(actfw_parallel_abort_##index) return ProcessCode::ABORT;    \
+   }
+
+/// Abort an enclosing ACTFW_PARALLEL_FOR construct and return to the
+/// enclosing scope.
+///
+/// @param index must match the index parameter given for that loop.
+///
+#define ACTFW_PARALLEL_FOR_BREAK(index)                              \
+   {                                                                 \
+      actfw_parallel_break_##index = true;                           \
+      ACTFW_PRAGMA(omp flush (actfw_parallel_break_##index))         \
+      continue;                                                      \
+   }
+
+/// Abort an enclosing ACTFW_PARALLEL_FOR and exit the host function by
+/// returning ProcessCode::ABORT.
+///
+/// @param index must match the index parameter given for that loop.
+///
+#define ACTFW_PARALLEL_FOR_ABORT(index)                              \
+   {                                                                 \
+      actfw_parallel_abort_##index = true;                           \
+      ACTFW_PARALLEL_FOR_BREAK(index)                                \
+   }
+
+// TODO: Once we can assume OpenMP 4.0 support from the host compiler,
+//       break out of the loop using #pragma omp cancel
+
+#endif  // ACTFW_CONCURRENCY_PARALLEL_FOR_H
