1356a6b33b93fd93b49e8055358788c50b59d797
Hadrien Grasland <grasland@lal.in2p3.fr>
2017-04-25 10:02:54 +0200

    Abort extrapolation reproducibility test if either run returns nonzero status

--- scripts/testExtrapolation.sh
+++ scripts/testExtrapolation.sh
@@ -5,10 +5,20 @@ rm -f ExtrapolationTest.root
 
 # Run the ExtrapolationExample in multi-threaded mode, back up the results
 ACTFWExtrapolationExample
+result=$?
+if [[ result -ne 0 ]]; then
+  echo "Multi-threaded extrapolation run failed!"
+  exit result
+fi
 mv ExtrapolationTest.root ExtrapolationTest_MT.root
 
 # Run the ExtrapolationExample in single-threaded mode, back up the results
 OMP_NUM_THREADS=1 ACTFWExtrapolationExample
+result=$?
+if [[ result -ne 0 ]]; then
+  echo "Single-threaded extrapolation run failed!"
+  exit result
+fi
 mv ExtrapolationTest.root ExtrapolationTest_ST.root
 
 # Check whether the results were identical (up to thread-induced event reordering)
