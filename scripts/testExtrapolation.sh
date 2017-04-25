062f33071224c953fc4266c856a556d08efcec1b
Hadrien Grasland <grasland@lal.in2p3.fr>
2017-04-25 11:55:18 +0200

    Made testExtrapolation a mere alias to testReproducibility

--- scripts/testExtrapolation.sh
+++ scripts/testExtrapolation.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-# Drop any stale result of the ExtrapolationExample
-rm -f ExtrapolationTest.root
-
-# Run the ExtrapolationExample in multi-threaded mode, back up the results
-ACTFWExtrapolationExample
-result=$?
-if [[ result -ne 0 ]]; then
-  echo "Multi-threaded extrapolation run failed!"
-  exit result
-fi
-mv ExtrapolationTest.root ExtrapolationTest_MT.root
-
-# Run the ExtrapolationExample in single-threaded mode, back up the results
-OMP_NUM_THREADS=1 ACTFWExtrapolationExample
-result=$?
-if [[ result -ne 0 ]]; then
-  echo "Single-threaded extrapolation run failed!"
-  exit result
-fi
-mv ExtrapolationTest.root ExtrapolationTest_ST.root
-
-# Check whether the results were identical (up to thread-induced event reordering)
-root -b -q -l -e '.X compareRootFiles.cpp("ExtrapolationTest_ST.root", "ExtrapolationTest_MT.root")'
-result=$?
-
-# Clean up, and return 0 if the results were identical
-rm -f ExtrapolationTest_MT.root ExtrapolationTest_ST.root
-exit $result
+# This is now just an alias to testReproducibility.sh
+./testReproducibility.sh Extrapolation
+exit $?
