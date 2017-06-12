fa9290440a27267114dd4694a91b515364611782
jhrdinka <julia.hrdinka@cern.ch>
2017-06-12 11:32:07 +0200

    Introduce Fatras example using particle gun

---
+++ Examples/FatrasParticleGun/src/DD4hepFatrasPGExample.cpp
@@ -0,0 +1,36 @@
+#include "ACTFW/Plugins/DD4hep/GeometryService.hpp"
+#include "ACTS/Detector/TrackingGeometry.hpp"
+#include "FatrasPGExampleBase.hpp"
+
+// the main executable
+int
+main(int argc, char* argv[])
+{
+  size_t nEvents = 1;
+
+  // get the DD4hep detector
+  // DETECTOR:
+  // --------------------------------------------------------------------------------
+  FWDD4hep::GeometryService::Config gsConfig("GeometryService",
+                                             Acts::Logging::INFO);
+
+  if (argc > 1) {
+    std::cout << "Creating detector from xml-file: '" << argv[1] << "'!"
+              << std::endl;
+    gsConfig.xmlFileName = argv[1];
+  } else
+    gsConfig.xmlFileName
+        = "file:Detectors/DD4hepDetector/compact/FCChhTrackerTkLayout.xml";
+  gsConfig.bTypePhi  = Acts::equidistant;
+  gsConfig.bTypeR    = Acts::equidistant;
+  gsConfig.bTypeZ    = Acts::equidistant;
+  gsConfig.envelopeR = 0.;
+  gsConfig.envelopeZ = 0.;
+
+  auto geometrySvc = std::make_shared<FWDD4hep::GeometryService>(gsConfig);
+  std::shared_ptr<const Acts::TrackingGeometry> dd4tGeometry
+      = geometrySvc->trackingGeometry();
+
+  // run the example
+  return ACTFWFatrasPGExample::run(nEvents, dd4tGeometry);
+}
