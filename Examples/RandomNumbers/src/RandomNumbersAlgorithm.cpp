f91e4e28433b083143f314727c0d0b31a06d1830
Hadrien Grasland <grasland@lal.in2p3.fr>
2017-06-22 17:40:16 +0200

    Ported random numbers example to STL distributions

--- Examples/RandomNumbers/src/RandomNumbersAlgorithm.cpp
+++ Examples/RandomNumbers/src/RandomNumbersAlgorithm.cpp
@@ -33,15 +33,22 @@ FW::ProcessCode
 FWE::RandomNumbersAlgorithm::execute(const FW::AlgorithmContext context) const
 {
   // Create a random number generator
-  FW::RandomNumbersSvc::Generator rng =
-    m_cfg.randomNumbers->spawnGenerator(context);
+  FW::RandomEngine rng = m_cfg.randomNumbers->spawnGenerator(context);
+
+  // Spawn some random number distributions
+  FW::GaussDist gaussDist(m_cfg.gaussParameters[0], m_cfg.gaussParameters[1]);
+  FW::UniformDist uniformDist(m_cfg.uniformParameters[0],
+                              m_cfg.uniformParameters[1]);
+  FW::LandauDist landauDist({m_cfg.uniformParameters, m_cfg.landauParameters});
+  FW::GammaDist gammaDist(m_cfg.gammaParameters[0], m_cfg.gammaParameters[1]);
+  FW::PoissonDist poissonDist(m_cfg.poissonParameter);
 
   for (size_t idraw = 0; idraw < m_cfg.drawsPerEvent; ++idraw) {
-    double gauss   = rng.drawGauss();
-    double uniform = rng.drawUniform();
-    double landau  = rng.drawLandau();
-    double gamma   = rng.drawGamma();
-    int    poisson = rng.drawPoisson();
+    double gauss   = gaussDist(rng);
+    double uniform = uniformDist(rng);
+    double landau  = landauDist(rng);
+    double gamma   = gammaDist(rng);
+    int    poisson = poissonDist(rng);
 
     ACTS_VERBOSE("Gauss   : " << gauss);
     ACTS_VERBOSE("Uniform : " << uniform);
