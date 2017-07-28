626517cce0d89cc4f7bd4c3602f028289c307ea4
msmk <msmk@cern.ch>
2017-07-28 09:00:07 +0200

    examples/helloworld: update interfaces

--- Examples/HelloWorld/src/HelloWorldAlgorithm.cpp
+++ Examples/HelloWorld/src/HelloWorldAlgorithm.cpp
@@ -2,38 +2,14 @@
 
 #include <iostream>
 
-FWE::HelloWorldAlgorithm::HelloWorldAlgorithm(const FW::Algorithm::Config& cnf)
-  : FW::Algorithm(cnf)
-{
-}
-
-FWE::HelloWorldAlgorithm::~HelloWorldAlgorithm()
+FWE::HelloWorldAlgorithm::HelloWorldAlgorithm()
+  : FW::BareAlgorithm("HelloWorld")
 {
 }
 
 FW::ProcessCode
-FWE::HelloWorldAlgorithm::initialize(std::shared_ptr<FW::WhiteBoard> jStore)
+FWE::HelloWorldAlgorithm::execute(FW::AlgorithmContext context) const
 {
-  // call the algorithm initialize for setting the stores
-  if (FW::Algorithm::initialize(jStore) != FW::ProcessCode::SUCCESS) {
-    ACTS_FATAL("Algorithm::initialize() did not succeed!");
-    return FW::ProcessCode::SUCCESS;
-  }
-  ACTS_VERBOSE("initialize successful.");
-  return FW::ProcessCode::SUCCESS;
-}
-
-FW::ProcessCode
-FWE::HelloWorldAlgorithm::execute(const FW::AlgorithmContext context) const
-{
-  ACTS_INFO(" Hello World! (from event " << context.eventContext->eventNumber << ")");
-  return FW::ProcessCode::SUCCESS;
-}
-
-/** Framework finalize mehtod */
-FW::ProcessCode
-FWE::HelloWorldAlgorithm::finalize()
-{
-  ACTS_VERBOSE("initialize successful.");
+  ACTS_INFO(" Hello World! (from event " << context.eventNumber << ")");
   return FW::ProcessCode::SUCCESS;
 }
--- Examples/HelloWorld/src/HelloWorldAlgorithm.hpp
+++ Examples/HelloWorld/src/HelloWorldAlgorithm.hpp
@@ -3,38 +3,23 @@
 
 #include <memory>
 
-#include "ACTFW/Framework/Algorithm.hpp"
-#include "ACTFW/Framework/AlgorithmContext.hpp"
+#include "ACTFW/Framework/BareAlgorithm.hpp"
 #include "ACTFW/Framework/ProcessCode.hpp"
-
-namespace FW {
-class WhiteBoard;
-}
 
 namespace FWE {
 
 /// @class Algorithm
-class HelloWorldAlgorithm : public FW::Algorithm
+class HelloWorldAlgorithm : public FW::BareAlgorithm
 {
 public:
   /// Constructor
-  HelloWorldAlgorithm(const FW::Algorithm::Config& cnf);
-
-  /// Destructor
-  ~HelloWorldAlgorithm();
-
-  /// Framework intialize method
-  FW::ProcessCode
-  initialize(std::shared_ptr<FW::WhiteBoard> jobStore = nullptr) final override;
+  HelloWorldAlgorithm();
 
   /// Framework execode method
   FW::ProcessCode
-  execute(const FW::AlgorithmContext context) const final override;
-
-  /// Framework finalize mehtod
-  FW::ProcessCode
-  finalize() final override;
+  execute(FW::AlgorithmContext context) const final override;
 };
-}
+
+}  // namespace FWE
 
 #endif  // ACTFW_EXAMPLES_HELLOWORLD_H
