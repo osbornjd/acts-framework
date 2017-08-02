3deb4a151c0c814ebf596db3cd1ab2bb8671f163
msmk <msmk@cern.ch>
2017-08-02 14:39:25 +0200

    examples/whiteboard: use event store by reference

--- Examples/WhiteBoard/src/WhiteBoardAlgorithm.cpp
+++ Examples/WhiteBoard/src/WhiteBoardAlgorithm.cpp
@@ -11,12 +11,8 @@ FWE::WhiteBoardAlgorithm::WhiteBoardAlgorithm(const Config& cfg)
 }
 
 FW::ProcessCode
-FWE::WhiteBoardAlgorithm::execute(FW::AlgorithmContext context) const
+FWE::WhiteBoardAlgorithm::execute(FW::AlgorithmContext ctx) const
 {
-  // Retrieve relevant information from the execution context
-  size_t eventNumber = context.eventNumber;
-  auto   eventStore  = context.eventStore;
-
   // -------- Reading -----------------------
   // Reading Class One
   if (!m_cfg.inputClassOneCollection.empty()) {
@@ -24,9 +20,8 @@ FWE::WhiteBoardAlgorithm::execute(FW::AlgorithmContext context) const
     // read in the collection
     const FWE::DataClassOneCollection* dcoCollIn = nullptr;
     // write to the EventStore
-    if (eventStore
-        && eventStore->get(m_cfg.inputClassOneCollection, dcoCollIn)
-            == FW::ProcessCode::ABORT)
+    if (ctx.eventStore.get(m_cfg.inputClassOneCollection, dcoCollIn)
+        == FW::ProcessCode::ABORT)
       return FW::ProcessCode::ABORT;
     // screen output
     ACTS_VERBOSE("Read DataClassOneCollection with size " << dcoCollIn->size());
@@ -40,9 +35,8 @@ FWE::WhiteBoardAlgorithm::execute(FW::AlgorithmContext context) const
     // read in the collection
     const FWE::DataClassTwoCollection* dctCollIn = nullptr;
     // write to the EventStore
-    if (eventStore
-        && eventStore->get(m_cfg.inputClassTwoCollection, dctCollIn)
-            == FW::ProcessCode::ABORT)
+    if (ctx.eventStore.get(m_cfg.inputClassTwoCollection, dctCollIn)
+        == FW::ProcessCode::ABORT)
       return FW::ProcessCode::ABORT;
     // screen output
     ACTS_VERBOSE("Read DataClassTwoCollection with size " << dctCollIn->size());
@@ -55,14 +49,13 @@ FWE::WhiteBoardAlgorithm::execute(FW::AlgorithmContext context) const
   if (!m_cfg.outputClassOneCollection.empty()) {
     ACTS_INFO("Writing ClassOneCollection " << m_cfg.outputClassOneCollection);
     // create a new collection
-    DataClassOneCollection dcoCollOut = {{"One", eventNumber}};
+    DataClassOneCollection dcoCollOut = {{"One", ctx.eventNumber}};
     ACTS_VERBOSE("Written out DataClassOne object as "
                  << dcoCollOut.back().data());
     // write to the EventStore
-    if (eventStore
-        && eventStore->add(m_cfg.outputClassOneCollection,
+    if (ctx.eventStore.add(m_cfg.outputClassOneCollection,
                            std::move(dcoCollOut))
-            == FW::ProcessCode::ABORT)
+        == FW::ProcessCode::ABORT)
       return FW::ProcessCode::ABORT;
   }
 
@@ -70,14 +63,13 @@ FWE::WhiteBoardAlgorithm::execute(FW::AlgorithmContext context) const
   if (!m_cfg.outputClassTwoCollection.empty()) {
     ACTS_INFO("Writing ClassTwoCollection " << m_cfg.outputClassTwoCollection);
     // create a new collection
-    DataClassTwoCollection dctCollOut = {{"Two", double(eventNumber)}};
+    DataClassTwoCollection dctCollOut = {{"Two", double(ctx.eventNumber)}};
     ACTS_VERBOSE("Written out DataClassTwo object as "
                  << dctCollOut.back().data());
     // write to the EventStore
-    if (eventStore
-        && eventStore->add(m_cfg.outputClassTwoCollection,
+    if (ctx.eventStore.add(m_cfg.outputClassTwoCollection,
                            std::move(dctCollOut))
-            == FW::ProcessCode::ABORT)
+        == FW::ProcessCode::ABORT)
       return FW::ProcessCode::ABORT;
   }
   // Return with success
--- Examples/WhiteBoard/src/WhiteBoardAlgorithm.hpp
+++ Examples/WhiteBoard/src/WhiteBoardAlgorithm.hpp
@@ -40,7 +40,7 @@ public:
 
   /// Framework execode method
   FW::ProcessCode
-  execute(FW::AlgorithmContext context) const final override;
+  execute(FW::AlgorithmContext ctx) const final override;
 
 private:
   Config m_cfg;
