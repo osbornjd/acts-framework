28f46cf7acceda2f92901b1a3e679417e4cce735
msmk <msmk@cern.ch>
2017-08-08 00:40:35 +0200

    root: update error/output handling in particle writer

--- Plugins/Root/src/RootParticleWriter.cpp
+++ Plugins/Root/src/RootParticleWriter.cpp
@@ -23,7 +23,6 @@ FW::Root::RootParticleWriter::initialize()
   }
   m_outputFile->cd();
   m_outputTree = new TTree(m_cfg.treeName.c_str(),
-                           "TTree from RootParticleWriter");
   // initial parameters
   m_outputTree->Branch("eta", &m_eta);
   m_outputTree->Branch("phi", &m_phi);
