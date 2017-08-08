c5638f72e04d5fbffdf2d24d846164814d4672c6
msmk <msmk@cern.ch>
2017-08-08 15:05:35 +0200

    particlegun: add d0,z0 as tunable parameters

--- Algorithms/Fatras/include/ACTFW/Fatras/ParticleGun.hpp
+++ Algorithms/Fatras/include/ACTFW/Fatras/ParticleGun.hpp
@@ -3,12 +3,15 @@
 
 #include <array>
 
+#include <ACTS/Utilities/Units.hpp>
+
 #include "ACTFW/Framework/BareAlgorithm.hpp"
 #include "ACTS/EventData/ParticleDefinitions.hpp"
 #include "ACTS/Utilities/Logger.hpp"
 
 namespace FW {
 
+class BarcodeSvc;
 class RandomNumbersSvc;
 
 /// @class ParticleGun
@@ -28,12 +31,18 @@ public:
     std::string particlesCollection;
     /// number of particles
     size_t nParticles = 0;
-    /// low, high for eta range
-    std::array<double, 2> etaRange = {{-3., 3.}};
+    /// low, high for d0 range
+    std::array<double, 2> d0Range = {{0, 1 * Acts::units::_mm}};
+    /// low, high for z0 range
+    std::array<double, 2> z0Range
+        = {{-100 * Acts::units::_mm, 100 * Acts::units::_mm}};
     /// low, high for phi range
     std::array<double, 2> phiRange = {{-M_PI, M_PI}};
+    /// low, high for eta range
+    std::array<double, 2> etaRange = {{-3., 3.}};
     /// low, high for pt range
-    std::array<double, 2> ptRange = {{100., 10000.}};
+    std::array<double, 2> ptRange
+        = {{100 * Acts::units::_MeV, 10 * Acts::units::_GeV}};
     /// the mass of the particle
     double mass = 0.;
     /// the charge of the particle
@@ -42,6 +51,7 @@ public:
     pdg_type pID = 0.;
     // FW random number service
     std::shared_ptr<FW::RandomNumbersSvc> randomNumbers = nullptr;
+    std::shared_ptr<FW::BarcodeSvc> barcodes = nullptr;
   };
 
   /// Constructor
--- Algorithms/Fatras/src/ParticleGun.cpp
+++ Algorithms/Fatras/src/ParticleGun.cpp
@@ -1,5 +1,8 @@
 #include "ACTFW/Fatras/ParticleGun.hpp"
 
+#include <cmath>
+
+#include "ACTFW/Barcode/BarcodeSvc.hpp"
 #include "ACTFW/Framework/WhiteBoard.hpp"
 #include "ACTFW/Random/RandomNumberDistributions.hpp"
 #include "ACTFW/Random/RandomNumbersSvc.hpp"
@@ -18,25 +21,28 @@ FW::ParticleGun::execute(AlgorithmContext ctx) const
 
   RandomEngine rng = m_cfg.randomNumbers->spawnGenerator(ctx);
 
+  UniformDist d0Dist(m_cfg.d0Range.at(0), m_cfg.d0Range.at(1));
+  UniformDist z0Dist(m_cfg.z0Range.at(0), m_cfg.z0Range.at(1));
   UniformDist phiDist(m_cfg.phiRange.at(0), m_cfg.phiRange.at(1));
   UniformDist etaDist(m_cfg.etaRange.at(0), m_cfg.etaRange.at(1));
   UniformDist ptDist(m_cfg.ptRange.at(0), m_cfg.ptRange.at(1));
 
   for (size_t ip = 0; ip < m_cfg.nParticles; ip++) {
     // generate random parameters
-    double phi   = phiDist(rng);
-    double eta   = etaDist(rng);
-    double theta = 2. * atan(exp(-eta));
-    double pt    = ptDist(rng);
-    double p     = pt / sin(theta);
+    double d0  = d0Dist(rng);
+    double z0  = z0Dist(rng);
+    double phi = phiDist(rng);
+    double eta = etaDist(rng);
+    double pt  = ptDist(rng);
+    auto   bc  = m_cfg.barcodes->generate(ip);
+    // create vertex from random parameters
+    Acts::Vector3D vertex(d0 * std::sin(phi), d0 * -std::cos(phi), z0);
     // create momentum from random parameters
     Acts::Vector3D momentum(
-        p * sin(theta) * cos(phi), p * sin(theta) * sin(phi), p * cos(theta));
-    // create vertex from random parameters
-    Acts::Vector3D vertex(0., 0., 0.);
+        pt * std::cos(phi), pt * std::sin(phi), pt * std::sinh(eta));
     // the particle should be ready now
     particles.emplace_back(
-        vertex, momentum, m_cfg.mass, m_cfg.charge, m_cfg.pID);
+        vertex, momentum, m_cfg.mass, m_cfg.charge, m_cfg.pID, bc);
   }
 
   if (ctx.eventStore.add(m_cfg.particlesCollection, std::move(particles))
