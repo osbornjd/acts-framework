70c92748c3a579905e824f19fb649b7929684c1c
jhrdinka <julia.hrdinka@cern.ch>
2017-08-21 18:06:53 +0200

    Introduce root writer for InterpolatedBFieldMap

---
+++ Plugins/Root/src/RootBFieldWriter.cpp
@@ -0,0 +1,24 @@
+#include "ACTFW/Plugins/Root/RootBFieldWriter.hpp"
+
+FWRoot::RootBFieldWriter::RootBFieldWriter(const Config& cfg)
+{
+}
+
+virtual FWRoot::RootBFieldWriter::~RootBFieldWriter()
+{
+}
+
+std::string
+FWRoot::RootBFieldWriter::name() const
+{
+}
+
+FW::ProcessCode
+FWRoot::RootBFieldWriter::initialize()
+{
+}
+
+FW::ProcessCode
+FWRoot::RootBFieldWriter::finalize()
+{
+}
