57056c0b00f954649e70f72603b41188fa5c4f29
jhrdinka <julia.hrdinka@cern.ch>
2017-09-28 11:02:53 +0200

    Introduce comparison type in compareMaterialEffects

--- scripts/compareMaterialEffects.cpp
+++ scripts/compareMaterialEffects.cpp
@@ -5,29 +5,31 @@
 #include "TROOT.h"
 #include "TTree.h"
 
-/// For comparison of the material effects for different pT.
+/// For comparison of the material effects for different values of a comparison
+/// type (e.g. pT, material thicknesses).
 /// It was created in order to validate Fatras simulation against Geant4.
 /// It should can used with the Output of the materialEffects.cpp script.
 
 void
 compareMaterialEffects(std::vector<std::string> dEHistFile_g4,
                        std::vector<std::string> dEHistFile_acts,
-                       std::vector<std::string> momenta,
+                       std::vector<std::string> comparison,
+                       std::string              comparisonType,
                        std::string              branchName,
                        std::string              histogramTitle)
 {
   if (dEHistFile_g4.size() != dEHistFile_acts.size()
-      || dEHistFile_g4.size() != momenta.size()
-      || dEHistFile_acts.size() != momenta.size()) {
+      || dEHistFile_g4.size() != comparison.size()
+      || dEHistFile_acts.size() != comparison.size()) {
     std::cout << "ERROR, Vectors need to have same size!" << std::endl;
     return;
   }
 
   TLegend* leg1 = new TLegend(0.2, 0.2, .4, .4);
-  leg1->SetHeader("pT");
+  leg1->SetHeader(comparisonType.c_str());
   gStyle->SetOptStat(0);
 
-  for (size_t it = 0; it < momenta.size(); it++) {
+  for (size_t it = 0; it < comparison.size(); it++) {
     std::cout << "Opening file: " << dEHistFile_g4.at(it).c_str() << std::endl;
     TFile inputFile_g4(dEHistFile_g4.at(it).c_str());
     TH1F* input_g4 = (TH1F*)inputFile_g4.Get(branchName.c_str());
@@ -51,9 +53,9 @@ compareMaterialEffects(std::vector<std::string> dEHistFile_g4,
     input_g4->SetLineStyle(it + 1);
     input_acts->SetLineStyle(it + 1);
 
-    input_acts->GetXaxis()->SetTitle("dz");
+    input_acts->GetXaxis()->SetTitle(branchName.c_str());
 
-    input_g4->GetXaxis()->SetTitle("dz");
+    input_g4->GetXaxis()->SetTitle(branchName.c_str());
 
     input_acts->SetTitle(histogramTitle.c_str());
     input_g4->SetTitle(histogramTitle.c_str());
@@ -68,7 +70,7 @@ compareMaterialEffects(std::vector<std::string> dEHistFile_g4,
       leg->Draw("same");
     }
 
-    leg1->AddEntry(input_g4, momenta.at(it).c_str());
+    leg1->AddEntry(input_g4, comparison.at(it).c_str());
 
     input_acts->SetDirectory(0);
     input_g4->SetDirectory(0);
