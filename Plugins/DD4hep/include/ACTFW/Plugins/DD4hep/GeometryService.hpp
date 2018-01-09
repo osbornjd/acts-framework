5a985cd7c36a10ee0973707700289357dca8d463
jhrdinka <julia.hrdinka@cern.ch>
2018-01-09 11:28:13 +0100

    Adapt DD4hep Geometry service to read in multiple xml files

--- Plugins/DD4hep/include/ACTFW/Plugins/DD4hep/GeometryService.hpp
+++ Plugins/DD4hep/include/ACTFW/Plugins/DD4hep/GeometryService.hpp
@@ -45,7 +45,7 @@ namespace DD4hep {
       /// The default logger
       std::shared_ptr<const Acts::Logger> logger;
       /// XML-file with the detector description
-      std::string xmlFileName;
+      std::vector<std::string> xmlFileNames;
       /// Logger for the geometry transformation
       Acts::Logging::Level lvl;
       /// The name of the service
@@ -91,7 +91,7 @@ namespace DD4hep {
       Config(const std::string&   lname = "GeometryService",
              Acts::Logging::Level level = Acts::Logging::INFO)
         : logger(Acts::getDefaultLogger(lname, level))
-        , xmlFileName("")
+        , xmlFileNames()
         , lvl(level)
         , name(lname)
         , bTypePhi(Acts::equidistant)
