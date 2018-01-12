92f7a771ddc7f2d1cbec4c8f6256182f2bcd64be
msmk <msmk@cern.ch>
2018-01-12 09:41:22 +0100

    cmake: FindPythia8 exports target

--- cmake/FindPythia8.cmake
+++ cmake/FindPythia8.cmake
@@ -1,36 +1,24 @@
-# Find the Pythia8 includes and library.
-# 
-# This module defines
-# PYTHIA8_INCLUDE_DIR, where to locate Pythia.h file
-# PYTHIA8_LIBRARIES, the libraries to link against to use Pythia6
-# PYTHIA8_FOUND.  If false, you cannot build anything that requires Pythia6.
-# PYTHIA8_LIBRARY, where to find the libpythia8 library.
+# Find the Pythia8 includes and libraries.
+#
+# This module defines the `Pythia8` imported target that encodes all
+# necessary information in its target properties.
 
-set(PYTHIA8_FOUND 0)
+find_library(Pythia8_LIBRARY
+  NAMES Pythia8 pythia8
+  HINTS ENV PYTHIA8_DIR
+  PATHS /opt/pythia8 /usr/local
+  DOC "The Pythia8 library")
+find_path(Pythia8_INCLUDE_DIR
+  NAMES Pythia8/Pythia.h
+  HINTS ENV PYTHIA8_DIR
+  PATHS /opt/pythia8 /usr/local
+  DOC "The Pythia8 include directory")
 
-find_path(PYTHIA8_INCLUDE_DIR Pythia.h
-  $ENV{PYTHIA8_DIR}/include
-  /opt/pythia8/include
-  /usr/local/include
-  /usr/include
-  /usr/include/pythia
-  DOC "Specify the directory containing Pythia.h."
-)
+find_package_handle_standard_args(Pythia8
+  REQUIRED_VARS Pythia8_LIBRARY Pythia8_INCLUDE_DIR)
 
-find_library(PYTHIA8_LIBRARY NAMES Pythia8 pythia8 PATHS
-  $ENV{PYTHIA8_DIR}/lib
-  /opt/pythia8/lib
-  /usr/local/lib
-  /usr/lib
-  DOC "Specify the Pythia8 library here."
-)
+add_library(Pythia8 SHARED IMPORTED)
+set_property(TARGET Pythia8 PROPERTY IMPORTED_LOCATION ${Pythia8_LIBRARY})
+set_property(TARGET Pythia8 PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${Pythia8_INCLUDE_DIR})
 
-if(PYTHIA8_INCLUDE_DIR AND PYTHIA8_LIBRARY)
-  set(PYTHIA8_FOUND 1 )
-  message(STATUS "Found Pythia8 library at ${PYTHIA8_LIBRARY}")
-endif()
-
-
-set(PYTHIA8_LIBRARIES ${PYTHIA8_LIBRARY})
-
-MARK_AS_ADVANCED( PYTHIA8_FOUND PYTHIA8_LIBRARY PYTHIA8_INCLUDE_DIR)
+mark_as_advanced(Pythia8_FOUND Pythia8_INCLUDE_DIR Pythia8_LIBRARY)
