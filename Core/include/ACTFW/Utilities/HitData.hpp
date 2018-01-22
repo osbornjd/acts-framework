87ad0f5ba9cd451454205102997cfe6070c6424f
jhrdinka <julia.hrdinka@cern.ch>
2018-01-22 18:16:18 +0100

    introduce fcc measurement

--- Core/include/ACTFW/Utilities/HitData.hpp
+++ Core/include/ACTFW/Utilities/HitData.hpp
@@ -7,11 +7,62 @@
 
 namespace FW {
 
-// Definition of a two dimensional measurement. The components are the local
+/// Definition of a two dimensional measurement. The components are the local
 // position on the surface.
 typedef Acts::
     Measurement<Identifier, Acts::ParDef::eLOC_0, Acts::ParDef::eLOC_1>
         Measurement2D;
+
+/// @classe fccMeasurement
+/// @brief Class to describe an fcc measurement
+class fccMeasurement
+{
+public:
+  // constructor
+  fccMeasurement(const Acts::Surface& surface,
+                 const Identifier&    id,
+                 Acts::Measurement<Identifier,
+                                   Acts::ParDef::eLOC_0,
+                                   Acts::ParDef::eLOC_1>::CovMatrix_t cov,
+                 const double&                                        loc0,
+                 const double&                                        loc1,
+                 const unsigned&                                      trackID)
+    : m_measurement(surface, id, cov, loc0, loc1), m_trackID(trackID)
+  {
+  }
+
+  // constructor using measurement
+  fccMeasurement(const Measurement2D& measurement, const unsigned& trackID)
+    : m_measurement(measurement), m_trackID(trackID)
+  {
+  }
+
+  ///@return measurement
+  const Measurement2D
+  measurement() const
+  {
+    return m_measurement;
+  }
+
+  ///@return local position of measurement
+  const Acts::Vector2D
+  locPos() const
+  {
+    return Acts::Vector2D(m_measurement.get<Acts::ParDef::eLOC_0>(),
+                          m_measurement.get<Acts::ParDef::eLOC_1>());
+  }
+
+  ///@return the trackID (identification of truth)
+  const unsigned
+  trackID() const
+  {
+    return m_trackID;
+  }
+
+private:
+  Measurement2D m_measurement;
+  unsigned      m_trackID;
+};
 
 /// @class AnalysisParameters
 ///
@@ -24,7 +75,7 @@ class AnalysisParameters
 {
 public:
   /// Constructor
-  AnalysisParameters(double mean, double min, double max)
+  AnalysisParameters(const double& mean, const double& min, const double& max)
     : m_mean(mean), m_min(min), m_max(max)
   {
   }
@@ -32,21 +83,21 @@ public:
   ~AnalysisParameters() = default;
 
   /// Access mean
-  const double&
+  const double
   mean() const
   {
     return m_mean;
   }
 
   /// Access min
-  const double&
+  const double
   min() const
   {
     return m_min;
   }
 
   /// Access max
-  const double&
+  const double
   max() const
   {
     return m_max;
