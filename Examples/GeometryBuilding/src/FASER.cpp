d9efd59d78efb71181765870c93b44b7c6bccd91
FabianKlimpel <fabian.klimpel@tum.de>
2018-03-16 11:07:07 +0100

    Tracking volume built. TODO: Vacuum between interaction point and detector. Finally a world volume can be created.

--- Examples/GeometryBuilding/src/FASER.cpp
+++ Examples/GeometryBuilding/src/FASER.cpp
@@ -22,6 +22,11 @@
 #include "ACTS/Tools/ITrackingVolumeHelper.hpp"
 #include "ACTS/Volumes/CuboidVolumeBounds.hpp"
 #include "ACTS/Material/Material.hpp"
+
+#include "ACTS/Tools/LayerArrayCreator.hpp"
+
+#include "ACTS/Utilities/BinningType.hpp"
+
 #include "ACTS/Utilities/BinnedArrayXD.hpp"
 #include "ACTS/Detector/TrackingVolume.hpp"
 
@@ -59,16 +64,16 @@ main(int argc, char* argv[])
 //---------------------------------------------------------
 
 //TODO: Units
-double halfX = 			5. * Acts::units::_m;
-double halfY = 			5. * Acts::units::_m;
-double thickness = 		0. * Acts::units::_mm;
-float X0 = 			95.7;
-float L0 = 			465.2;
-float A = 			28.03;
-float Z = 			14.;
-float Rho = 			2.32e-3;
-unsigned int numLayers = 	5;
-double localPos[] =		{0. * Acts::units::_mm,
+const double halfX = 			5. * Acts::units::_m;
+const double halfY = 			5. * Acts::units::_m;
+const double thickness = 		0. * Acts::units::_mm;
+const float X0 = 			95.7;
+const float L0 = 			465.2;
+const float A = 			28.03;
+const float Z = 			14.;
+const float Rho = 			2.32e-3;
+const unsigned int numLayers = 	5;
+std::array<double, numLayers> localPos =		{0. * Acts::units::_mm,
 				5. * Acts::units::_mm,
 				10. * Acts::units::_mm,
 				15. * Acts::units::_mm,
@@ -79,8 +84,8 @@ double posFirstSur =		150. * Acts::units::_m;
 //Build Surfaces
 std::shared_ptr<const Acts::RectangleBounds> recBounds(new Acts::RectangleBounds(halfX, halfY));
 
-Acts::Transform3D t3d[numLayers];
-Acts::PlaneSurface* pSur[numLayers];
+std::array<Acts::Transform3D, numLayers> t3d;
+std::array<Acts::PlaneSurface*, numLayers> pSur;
 for(unsigned int iLayer = 0; iLayer < numLayers; iLayer++) 
 {
     t3d[iLayer] = Acts::Translation3D(0., 0., posFirstSur + localPos[iLayer]);
@@ -90,8 +95,8 @@ for(unsigned int iLayer = 0; iLayer < numLayers; iLayer++)
 }
 
 //Build Layers
-std::unique_ptr<Acts::SurfaceArray> surArrays[numLayers];
-Acts::LayerPtr layPtr[numLayers];
+std::array<std::unique_ptr<Acts::SurfaceArray>, numLayers> surArrays;
+std::array<Acts::LayerPtr, numLayers> layPtr;
 for(unsigned int iSurface; iSurface < numLayers; iSurface++)
 {
     surArrays[iSurface] = std::make_unique<Acts::SurfaceArray>(Acts::SurfaceArray(pSur[iSurface]));
@@ -103,39 +108,34 @@ for(unsigned int iSurface; iSurface < numLayers; iSurface++)
 }
 
 //Build Volumes
-Acts::VolumeBoundsPtr volBoundsPtr[numLayers];
-std::shared_ptr<Acts::Material> mat[numLayers];
-for(unsigned int iVolume; iVolume < numLayers; iVolume++)
-{
-    volBoundsPtr[iVolume] = std::make_shared<const Acts::CuboidVolumeBounds>(
-			    Acts::CuboidVolumeBounds(halfX,
-			    halfY,
-			    0.5 * thickness));
+Acts::Transform3D trans;
+trans = Acts::Translation3D(0., 0., posFirstSur + (localPos.front() + localPos.back()) / 2);
+std::shared_ptr<const Acts::Transform3D> htrans(&trans);
 
-    mat[iVolume] = std::make_shared<Acts::Material>(Acts::Material(
-		    X0,
-		    L0,
-		    A,
-		    Z,
-		    Rho));
-}
+Acts::VolumeBoundsPtr volBoundsPtr = std::make_shared<const Acts::CuboidVolumeBounds>(
+					Acts::CuboidVolumeBounds(halfX,
+								halfY,
+								localPos.front() + localPos.back()));
 
-std::vector<std::pair<Acts::LayerPtr, Acts::Vector3D>> tapVec;
-for(unsigned int iVolume; iVolume < numLayers; iVolume++)
-{
-    tapVec.push_back(std::pair<Acts::LayerPtr, Acts::Vector3D>(layPtr[iVolume], Acts::Vector3D(0., 0., posFirstSur + localPos[iVolume])));
-}
-
-//std::unique_ptr<const Acts::LayerArray> layArr(new Acts::BinnedArrayXD<Acts::LayerPtr>());;
+std::shared_ptr<const Acts::Material> mat(new Acts::Material(X0, L0, A, Z, Rho));
 
 
-//std::unique_ptr<const Acts::LayerArray> binArrXD(new Acts::BinnedArrayXD<Acts::LayerPtr>(mlp));
+Acts::LayerVector layVec;
+for(auto layer : layPtr) layVec.push_back(layer);
 
-//const Acts::LayerVector layVec(1, mlp);
+Acts::LayerArrayCreator layArrCreator;
+std::unique_ptr<const Acts::LayerArray> layArr(layArrCreator.layerArray(
+							layVec,
+							posFirstSur + localPos.front(), //TODO: Epsilon Umgebung
+							posFirstSur + localPos.back(),
+							Acts::BinningType::arbitrary,
+							Acts::BinningValue::binZ));
 
-
-//TODO: Transform3D direkt als shared_ptr erzeugen
-//Acts::MutableTrackingVolumePtr mtvp(Acts::TrackingVolume::create(std::make_shared<const Acts::Transform3D>(t3d), vbp, mat, std::move(binArrXD), layVec));
+Acts::MutableTrackingVolumePtr mtvp(Acts::TrackingVolume::create(htrans,
+								volBoundsPtr, 
+								mat, 
+								std::move(layArr), 
+								layVec));
 
 //Acts::TrackingGeometry tGeo(mtvp);
 
