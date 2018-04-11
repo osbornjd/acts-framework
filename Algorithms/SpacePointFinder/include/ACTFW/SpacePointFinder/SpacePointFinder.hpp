4c0df8ff62d4bfac09f4c80e743b6a653b2788ca
FabianKlimpel <fabian.klimpel@tum.de>
2018-04-11 15:00:10 +0200

    First version. Some robustness/general parts missing.

---
+++ Algorithms/SpacePointFinder/include/ACTFW/SpacePointFinder/SpacePointFinder.hpp
@@ -0,0 +1,148 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2017 ACTS project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef ACTFW_ALGORITHMS_SPACEPOINTFINDER_H
+#define ACTFW_ALGORITHMS_SPACEPOINTFINDER_H
+
+#include <ACTS/Utilities/Units.hpp>
+#include <array>
+#include "ACTFW/Framework/BareAlgorithm.hpp"
+#include "ACTS/Utilities/Logger.hpp"
+#include "ACTFW/EventData/DataContainers.hpp"
+#include <ACTS/Digitization/PlanarModuleCluster.hpp>
+#include "ACTS/Digitization/DigitizationModule.hpp"
+#include "ACTFW/GenericDetector/GenericDetectorElement.hpp"
+#include "ACTS/Digitization/CartesianSegmentation.hpp"
+
+namespace FW {
+
+/// Type of the hit data stored in the detector
+typedef DetectorData<geo_id_value, Acts::PlanarModuleCluster> DetData;
+
+/// @class SpacePointFinder
+///
+/// After the particle interaction with surfaces are recorded and digitized
+/// the hits strip detectors need further treatment. This class takes
+/// the digitized hits and combines them on at least two different
+/// strips to a result of the combined detector element.
+///
+
+class SpacePointFinder : public BareAlgorithm
+{
+public:
+  struct Config
+  {
+    /// Input collection of digitized particles
+    std::string collectionIn;
+    /// Output collection of calculated hit coordinates
+    std::string collectionOut;
+    /// Accepted difference in eta for two hits
+    double diffTheta2 = 1.;
+    /// Accepted difference in phi for two hits
+    double diffPhi2 = 1.;
+    /// Accepted distance between two hits
+    double diffDist = 100.;
+    /// Assumed position of the vertex
+    Acts::Vector3D vertex = {0., 0., 0.};
+  };
+  
+  /// Structure for easier bookkeeping of potential hit combinations
+  /// on two surfaces
+  struct CombinedHits
+  {
+	/// Storage of the hit on the first surface
+	Acts::PlanarModuleCluster const * hitModule1;
+	/// Storage of the hit on the second surface
+	Acts::PlanarModuleCluster const * hitModule2;
+	/// Storage of (Delta eta)^2 + (Delta phi)^2
+	/// Allows vetos between different possible combinations
+	double diff;	  
+  };
+
+  /// Constructor
+  /// @param cfg is the configuration class
+  SpacePointFinder(const Config&        cfg,
+              Acts::Logging::Level level = Acts::Logging::INFO);
+
+  /// Executes the full algorithm. Extracts the data from Whiteboard,
+  /// processes it and stores the space points on the Whiteboard.
+  /// @param ctx is the Whiteboard that stores the data
+  /// @return information is the algorithm was successful
+  ProcessCode
+  execute(AlgorithmContext ctx) const;
+
+private:
+  Config m_cfg;
+  
+  /// Getter method for the digitized hits
+  /// @param ctx is the reference to the Whiteboard that stores the data
+  /// @param detData local pointer to all produced digitized hits
+  /// @return information if the data was found or not
+  ProcessCode 
+  clusterReading(AlgorithmContext& ctx, const DetData*& detData) const;
+  
+  /// Getter method for the local coordinates of a hit 
+  /// on its corresponding surface
+  /// @param hit object related to the hit that holds the necessary information
+  /// @return vector of the local coordinates of the hit on the surface
+  const 
+  Acts::Vector2D localCoords(const Acts::PlanarModuleCluster& hit) const;
+  
+  /// Getter method for the global coordinates of a hit 
+  /// @param hit object related to the hit that holds the necessary information
+  /// @return vector of the global coordinates of the hit
+  const 
+  Acts::Vector3D globalCoords(const Acts::PlanarModuleCluster& hit) const;
+  
+  /// Calculates (Delta theta)^2 + (Delta phi)^2 between two hits
+  /// @param hit1 the first hit
+  /// @param hit2 the second hit
+  /// @return the squared sum in case of success, otherwise -1
+  const double 
+  differenceOfHits(const Acts::PlanarModuleCluster& hit1, 
+				const Acts::PlanarModuleCluster& hit2) const;
+    
+  /// Searches possible combinations of two hits on different surfaces
+  /// that may come from the same particles
+  /// @param vec1 vector of hits on the first surface
+  /// @param vec2 vector of hits on the second surface
+  /// @return structure that stores all candidates of combined hits
+  std::vector<CombinedHits> 
+  combineHits(const std::vector<Acts::PlanarModuleCluster>& vec1, 
+			const std::vector<Acts::PlanarModuleCluster>& vec2) const;
+  
+  /// Searches and stores all hit combination candidates in the detector
+  /// @param detData local pointer to all produced digitized hits
+  /// @param allCombHits matrix that stores all hit combination candidates
+  void 
+  findOverlappingClusters(const DetData*& detData, 
+					std::vector<std::vector<CombinedHits>>& allCombHits) const;
+
+  /// No function, still TODO
+  void 
+  filterCombinations(std::vector<std::vector<CombinedHits>>& allCombHits) const;
+  
+  /// Calculates the top and bottom ends of a strip detector element
+  /// that corresponds to a given hit
+  /// @param hit object that stores the information about the hit
+  /// @return vectors to the top and bottom end of the strip detector element
+  const std::pair<Acts::Vector3D, Acts::Vector3D> 
+  endsOfStrip(const Acts::PlanarModuleCluster& hit) const;
+    
+  /// Calculates the space points out of a given collection of hits
+  /// on several strip detectors
+  /// @param allCombHits matrix that stores all hit combination candidates
+  /// @return pointer to all resolved space points
+  std::unique_ptr<const std::vector<Acts::Vector3D>> 
+  calculateSpacePoints(
+					std::vector<std::vector<CombinedHits>>& allCombHits) const;
+};
+
+}  // namespace FW
+
+#endif  // ACTFW_ALGORITHMS_SPACEPOINTFINDER_H
---
+++ Algorithms/SpacePointFinder/include/ACTFW/SpacePointFinder/SpacePointFinderOptions.hpp
@@ -0,0 +1,99 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2017 ACTS project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef ACTFW_OPTIONS_PARTICLEGUNOPTIONS_HPP
+#define ACTFW_OPTIONS_PARTICLEGUNOPTIONS_HPP
+
+#include <cstdlib>
+#include <iostream>
+#include <utility>
+#include "ACTFW/ParticleGun/ParticleGun.hpp"
+#include "ACTFW/Utilities/Options.hpp"
+#include "ACTS/Utilities/Units.hpp"
+
+namespace po = boost::program_options;
+
+namespace au = Acts::units;
+
+namespace FW {
+
+namespace Options {
+
+  /// the particle gun options, the are prefixes with gp
+  template <class AOPT>
+  void
+  addParticleGunOptions(AOPT& opt)
+  {
+    opt.add_options()("pg-nparticles",
+                      po::value<size_t>()->default_value(100.),
+                      "number of particles.")(
+        "pg-pdg",
+        po::value<int>()->default_value(13),
+        "PDG number of the particle, will be adjusted for charge flip.")(
+        "pg-mass",
+        po::value<double>()->default_value(105.),
+        "mass of the particle in [MeV]")(
+        "pg-charge",
+        po::value<double>()->default_value(-1.),
+        "charge of the particle in [e]")(
+        "pg-chargeflip",
+        po::value<bool>()->default_value(true),
+        "flip the charge (and change PDG accordingly).")(
+        "pg-d0range",
+        po::value<read_range>()->multitoken()->default_value({0., 0.}),
+        "range in which the d0 parameter is simulated in [mm]. Please hand"
+        "over by simply seperating the values by space")(
+        "pg-z0range",
+        po::value<read_range>()->multitoken()->default_value({0., 0.}),
+        "range in which the z0 parameter is simulated in [mm]. Please hand"
+        "over by simply seperating the values by space")(
+        "pg-phirange",
+        po::value<read_range>()->multitoken()->default_value({-M_PI, M_PI}),
+        "range in which the phi0 parameter is simulated. Please hand over by "
+        "simply seperating the values by space")(
+        "pg-etarange",
+        po::value<read_range>()->multitoken()->default_value({-4., 4.}),
+        "range in which the eta parameter is simulated. Please hand over by "
+        "simply seperating the values by space")(
+        "pg-ptrange",
+        po::value<read_range>()->multitoken()->default_value({100., 1e5}),
+        "range in which the pt in [MeV] parameter is simulated. Please hand "
+        "over by simply seperating the values by space");
+  }
+
+  /// read the particle gun options and return a Config file
+  template <class AMAP>
+  FW::ParticleGun::Config
+  readParticleGunConfig(const AMAP& vm)
+  {
+    // read the reange as vector (missing istream for std::array)
+    auto d0r  = vm["pg-d0range"].template as<read_range>();
+    auto z0r  = vm["pg-z0range"].template as<read_range>();
+    auto phir = vm["pg-phirange"].template as<read_range>();
+    auto etar = vm["pg-etarange"].template as<read_range>();
+    auto ptr  = vm["pg-ptrange"].template as<read_range>();
+    // particle gun as generator
+    FW::ParticleGun::Config particleGunConfig;
+    particleGunConfig.evgenCollection = "EvgenParticles";
+    particleGunConfig.nParticles = vm["pg-nparticles"].template as<size_t>();
+    particleGunConfig.d0Range    = {{d0r[0] * au::_mm, d0r[1] * au::_mm}};
+    particleGunConfig.z0Range    = {{z0r[0] * au::_mm, z0r[1] * au::_mm}};
+    particleGunConfig.phiRange   = {{phir[0], phir[1]}};
+    particleGunConfig.etaRange   = {{etar[0], etar[1]}};
+    particleGunConfig.ptRange    = {{ptr[0] * au::_MeV, ptr[1] * au::_MeV}};
+    particleGunConfig.mass   = vm["pg-mass"].template as<double>() * au::_MeV;
+    particleGunConfig.charge = vm["pg-charge"].template as<double>() * au::_e;
+    particleGunConfig.randomCharge = vm["pg-chargeflip"].template as<bool>();
+    particleGunConfig.pID          = vm["pg-pdg"].template as<int>();
+    // return the config object
+    return particleGunConfig;
+  }
+}
+}
+
+#endif  // ACTFW_OPTIONS_PARTICLEGUNOPTIONS_HPP
