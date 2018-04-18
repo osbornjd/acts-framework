cb6f307650b4d55400cc3480ea0755f8dd5f4595
FabianKlimpel <fabian.klimpel@tum.de>
2018-04-18 09:21:31 +0200

    Added stability parts in space point formation

--- Algorithms/SpacePointFinder/include/ACTFW/SpacePointFinder/SpacePointFinder.hpp
+++ Algorithms/SpacePointFinder/include/ACTFW/SpacePointFinder/SpacePointFinder.hpp
@@ -31,6 +31,7 @@ typedef DetectorData<geo_id_value, Acts::PlanarModuleCluster> DetData;
 /// the digitized hits and combines them on at least two different
 /// strips to a result of the combined detector element.
 ///
+/// @note Used abbreviation: "Strip Detector Element" -> SDE
 /// @note This is a rough implementation underlying many assumptions
 ///
 class SpacePointFinder : public BareAlgorithm
@@ -49,21 +50,12 @@ public:
     double diffPhi2 = 1.;
     /// Accepted distance between two hits
     double diffDist = 100.;
+    /// Allowed increase of strip length
+    double stripLengthTolerance = 0.01;
+    /// Allowed increase of strip length wrt gaps between strips
+    double stripLengthGapTolerance = 0.01;
     /// Assumed position of the vertex
     Acts::Vector3D vertex = {0., 0., 0.};
-  };
-
-  /// @brief Structure for easier bookkeeping of potential hit combinations
-  /// on two surfaces
-  struct CombinedHits
-  {
-    /// Storage of the hit on the first surface
-    Acts::PlanarModuleCluster const* hitModule1;
-    /// Storage of the hit on the second surface
-    Acts::PlanarModuleCluster const* hitModule2;
-    /// Storage of (Delta eta)^2 + (Delta phi)^2
-    /// Allows vetos between different possible combinations
-    double diff;
   };
 
   /// Constructor
@@ -79,6 +71,66 @@ public:
   execute(AlgorithmContext ctx) const;
 
 private:
+
+  /// @brief Structure for easier bookkeeping of potential hit combinations
+  /// on two surfaces
+  struct CombinedHits
+  {
+    /// Storage of the hit on the first surface
+    Acts::PlanarModuleCluster const* hitModule1;
+    /// Storage of the hit on the second surface
+    Acts::PlanarModuleCluster const* hitModule2;
+    /// Storage of (Delta eta)^2 + (Delta phi)^2
+    /// Allows vetos between different possible combinations
+    double diff;
+  };
+  
+  /// @brief Storage container for variables related to the calculation of space points
+  struct SpacePointParameters
+  {
+	/// Vector pointing from bottom to top end of first SDE
+	Acts::Vector3D 	q;
+	/// Vector pointing from bottom to top end of second SDE
+	Acts::Vector3D 	r;
+	/// Twice the vector pointing from vertex to to midpoint of first SDE
+	Acts::Vector3D 	s;
+	/// Twice the vector pointing from vertex to to midpoint of second SDE
+	Acts::Vector3D 	t;
+	/// Cross product between SpacePointParameters::q and SpacePointParameters::s
+	Acts::Vector3D 	qs;
+	/// Cross product between SpacePointParameters::r and SpacePointParameters::t
+	Acts::Vector3D 	rt;
+	/// Magnitude of SpacePointParameters::q
+	double 			qmag = 0.;
+	/// Parameter that determines the hit position on the first SDE
+	double		  	m = 0.;
+	/// Parameter that determines the hit position on the second SDE
+	double		  	n = 0.;
+	/// Regular limit of the absolut values of SpacePointParameters::m and SpacePointParameters::n
+	double		  	limit = 1.;
+	/// Limit of SpacePointParameters::m and SpacePointParameters::n in case of variable vertex
+	double			limitExtended = 1.;
+	
+	/// @brief reset structure and allows to reuse the same structure
+	void reset()
+	{
+		// Set every vector to nullvector. This allows checks, if a variable was already set.
+		q 				= {0., 0., 0.};
+		r 				= {0., 0., 0.};
+		s 				= {0., 0., 0.};
+		t 				= {0., 0., 0.};
+		qs				= {0., 0., 0.};
+		rt				= {0., 0., 0.};
+		// Set every double to default values
+		qmag 			= 0;
+		m 				= 0;
+		n 				= 0;
+		limit 			= 1.;
+		limitExtended 	= 1.;
+	}
+  };
+  
+  /// Storage of the user defined configuration of the class
   Config m_cfg;
 
   /// @brief Getter method for the digitized hits
@@ -113,10 +165,11 @@ private:
   /// that may come from the same particles
   /// @param vec1 vector of hits on the first surface
   /// @param vec2 vector of hits on the second surface
-  /// @return structure that stores all candidates of combined hits
-  std::vector<CombinedHits>
+  /// @param combHits structure that stores all candidates of combined hits
+  void
   combineHits(const std::vector<Acts::PlanarModuleCluster>& vec1,
-              const std::vector<Acts::PlanarModuleCluster>& vec2) const;
+              const std::vector<Acts::PlanarModuleCluster>& vec2,
+              std::vector<CombinedHits>&					combHits) const;
 
   /// @brief Searches and stores all hit combination candidates in the detector
   /// @param detData local pointer to all produced digitized hits
@@ -130,19 +183,34 @@ private:
   void
   filterCombinations(std::vector<std::vector<CombinedHits>>& allCombHits) const;
 
-  /// @brief Calculates the top and bottom ends of a strip detector element
+  /// @brief Calculates the top and bottom ends of a SDE
   /// that corresponds to a given hit
   /// @param hit object that stores the information about the hit
-  /// @return vectors to the top and bottom end of the strip detector element
+  /// @return vectors to the top and bottom end of the SDE
   std::pair<Acts::Vector3D, Acts::Vector3D>
   endsOfStrip(const Acts::PlanarModuleCluster& hit) const;
 
+  /// @brief This function tests if a space point can be estimated by a more tolerant treatment of construction. In fact, this function indirectly allows shifts of the vertex.
+  /// @param spaPoPa container that stores geometric parameters and rules of the space point formation
+  /// @return indicator if the test was successful
+  bool
+  recoverSpacePoint(SpacePointParameters& spaPoPa) const;
+  
+  /// @brief Stores a calculated space point
+  /// @param spacePoint location of the determined space point on the first SDE
+  /// @param hits data container that provides additional information about the space point
+  /// @param stripClust data container that stores all information
+  void
+  storeSpacePoint(const Acts::Vector3D& spacePoint,
+				  const CombinedHits&	hits,
+				  DetData& 				stripClusters) const;
+	
   /// @brief Calculates the space points out of a given collection of hits
   /// on several strip detectors and stores the data
   /// @param allCombHits matrix that stores all hit combination candidates
   /// @param detData local pointer to all produced digitized hits
   /// @return pointer to all resolved space points
-  FW::DetectorData<geo_id_value, Acts::PlanarModuleCluster>
+  DetData
   calculateSpacePoints(std::vector<std::vector<CombinedHits>>& allCombHits,
                        const DetData*&                         detData) const;
 };
--- Algorithms/SpacePointFinder/src/SpacePointFinder.cpp
+++ Algorithms/SpacePointFinder/src/SpacePointFinder.cpp
@@ -12,6 +12,10 @@
 #include <stdexcept>
 #include "ACTFW/Framework/WhiteBoard.hpp"
 
+///
+/// @note Used abbreviation: "Strip Detector Element" -> SDE
+///
+
 FW::SpacePointFinder::SpacePointFinder(const FW::SpacePointFinder::Config& cfg,
                                        Acts::Logging::Level level)
   : FW::BareAlgorithm("SpacePointFinder", level), m_cfg(cfg)
@@ -134,17 +138,17 @@ FW::SpacePointFinder::differenceOfHits(
   return diffTheta2 + diffPhi2;
 }
 
-std::vector<FW::SpacePointFinder::CombinedHits>
+void
 FW::SpacePointFinder::combineHits(
     const std::vector<Acts::PlanarModuleCluster>& vec1,
-    const std::vector<Acts::PlanarModuleCluster>& vec2) const
+    const std::vector<Acts::PlanarModuleCluster>& vec2,
+    std::vector<FW::SpacePointFinder::CombinedHits>& combHits) const
 {
   // TODO: only the closest differences get selected -> some points are not
   // taken into account
   // Declare helper variables
   double                                          currentDiff;
   FW::SpacePointFinder::CombinedHits              tmpCombHits;
-  std::vector<FW::SpacePointFinder::CombinedHits> combHits;
   double                                          diffMin;
   unsigned int                                    hitMin;
 
@@ -171,9 +175,6 @@ FW::SpacePointFinder::combineHits(
       combHits.push_back(tmpCombHits);
     }
   }
-
-  // Return the list of closest hits
-  return combHits;
 }
 
 void
@@ -183,35 +184,46 @@ FW::SpacePointFinder::findOverlappingClusters(
     const
 {
   // Declare temporary storage
-  std::vector<Acts::PlanarModuleCluster> const* module1;
-  std::vector<Acts::PlanarModuleCluster> const* module2;
-  // TODO: Only treats two surfaces per layer
+  std::vector<std::vector<Acts::PlanarModuleCluster> const*> modules;
+  unsigned int index;
+  std::vector<FW::SpacePointFinder::CombinedHits> combHits;
 
   // Loop over the planar clusters in this event
   for (auto& volumeData : *detData)
     for (auto& layerData : volumeData.second) {
-      // Set to nullptr as veto
-      module1 = nullptr;
-      module2 = nullptr;
+      modules.clear();
+      combHits.clear();
 
       for (auto& moduleData : layerData.second) {
-        // Store the Hits of the 1st and 2nd surface
-        if (moduleData.first == 1)
-          module1 = &moduleData.second;
-        else if (moduleData.first == 2)
-          module2 = &moduleData.second;
-        else {
-          ACTS_INFO("Warning: More than two Surfaces not implemented yet");
-          return;
-        }
+		index = moduleData.first - 1;
+		if(moduleData.first + 1 >= modules.size())
+			modules.resize(index + 1);
+		// Store the data 
+		modules[index] = &(moduleData.second);
       }
-      // Get the combination of hits
-      if (module1 && module2) {
-        std::vector<FW::SpacePointFinder::CombinedHits> combHits
-            = combineHits(*module1, *module2);
-        allCombHits.push_back(std::move(combHits));
-      } else
-        ACTS_VERBOSE("Layer " << layerData.first << " stored no surfaces");
+      switch(modules.size()) {
+		// Easy exit if no surfaces are available
+		case 0 : ACTS_VERBOSE("Layer " << layerData.first << " stored no surfaces");
+				 break;
+		// Easy exit if only a single surface is available 
+		case 1 : ACTS_VERBOSE("Layer " << layerData.first << " stored a single surface");
+				 break;
+		// Form combinations between all hits on both surfaces
+		case 2 : combineHits(*(modules[0]), *(modules[1]), combHits);
+				 break;
+		// Try to combine the hits of every surface with each other
+		default : std::vector<FW::SpacePointFinder::CombinedHits> tmpCombHits;
+				  for(unsigned int i = 0; i < modules.size(); i++)
+					for(unsigned int j = i + 1; j < modules.size(); j++)
+					{
+						combineHits(*(modules[i]), *(modules[j]), tmpCombHits);
+						combHits.insert(combHits.end(), 
+										tmpCombHits.begin(), 
+										tmpCombHits.end());
+				    }	  
+	  }
+	  // Store the found combination candidates
+	  allCombHits.push_back(std::move(combHits));
     }
 }
 
@@ -282,7 +294,102 @@ FW::SpacePointFinder::endsOfStrip(const Acts::PlanarModuleCluster& hit) const
   return std::make_pair(topGlobal, bottomGlobal);
 }
 
-//~ std::unique_ptr<const std::vector<Acts::Vector3D>>
+bool
+FW::SpacePointFinder::recoverSpacePoint(
+	FW::SpacePointFinder::SpacePointParameters& spaPoPa) const
+{
+	/// Consider some cases that would allow an easy exit
+	// Check if the limits are allowed to be increased
+	if(m_cfg.stripLengthGapTolerance <= 0.) return false;
+	spaPoPa.qmag  = spaPoPa.q.mag();
+	// Increase the limits. This allows a check if the point is just slightly outside the SDE
+    spaPoPa.limitExtended = spaPoPa.limit + m_cfg.stripLengthGapTolerance / spaPoPa.qmag;
+    // Check if m is just slightly outside
+    if(fabs(spaPoPa.m) > spaPoPa.limitExtended) return false;
+    //Calculate n if not performed previously
+    if(spaPoPa.n == 0.)
+		spaPoPa.n = -spaPoPa.t.dot(spaPoPa.qs)
+					/ spaPoPa.r.dot(spaPoPa.qs);
+	// Check if n is just slightly outside				
+    if(fabs(spaPoPa.n) > spaPoPa.limitExtended) return false;
+    
+	/// The following code considers an overshoot of m and n in the same direction of their SDE. The term "overshoot" represents the amount of m or n outside its regular interval (-1, 1).
+	/// It calculates which overshoot is worse. In order to compare both, the overshoot in n is projected onto the first surface by considering the normalized projection of r onto q.
+	/// This allows a rescaling of the overshoot. The worse overshoot will be set to +/-1, the parameter with less overshoot will be moved towards 0 by the worse overshoot.
+	/// In order to treat both SDEs equally, the rescaling eventually needs to be performed several times. If these shifts allows m and n to be in the limits, the space point can be stored.
+	/// @note This shift can be understood as a shift of the particle's trajectory. This is leads to a shift of the vertex. Since these two points are treated independently from other measurement, it is also possible to consider this as a change in the slope of the particle's trajectory. The would also move the vertex position.
+    
+    // Calculate the scaling factor to project lengths of the second SDE on the first SDE
+    double secOnFirstScale = spaPoPa.q.dot(spaPoPa.r) / (spaPoPa.qmag * spaPoPa.qmag);
+    // Check if both overshoots are in the same direction 
+    if(spaPoPa.m > 1. && spaPoPa.n > 1.)
+    {
+		// Calculate the overshoots
+		double mOvershoot = spaPoPa.m - 1.;
+		double nOvershoot = (spaPoPa.n - 1.) * secOnFirstScale; // Perform projection
+		// Resolve worse overshoot
+		double biggerOvershoot = (mOvershoot > nOvershoot) ? mOvershoot : nOvershoot;
+		// Move m and n towards 0
+		spaPoPa.m -= biggerOvershoot;
+		spaPoPa.n -= (biggerOvershoot / secOnFirstScale);
+		// Check if this recovered the space point
+		return fabs(spaPoPa.m) < spaPoPa.limit && fabs(spaPoPa.n) < spaPoPa.limit;
+	}
+	// Check if both overshoots are in the same direction 
+	if(spaPoPa.m < -1. && spaPoPa.n < -1.)
+    {
+		// Calculate the overshoots
+		double mOvershoot = -(spaPoPa.m + 1.);
+		double nOvershoot = -(spaPoPa.n + 1.) * secOnFirstScale; // Perform projection
+		// Resolve worse overshoot
+		double biggerOvershoot = (mOvershoot > nOvershoot) ? mOvershoot : nOvershoot;
+		// Move m and n towards 0
+		spaPoPa.m += biggerOvershoot;
+		spaPoPa.n += (biggerOvershoot / secOnFirstScale);
+		// Check if this recovered the space point
+		return fabs(spaPoPa.m) < spaPoPa.limit && fabs(spaPoPa.n) < spaPoPa.limit;
+	}
+	// No solution could be found
+    return false;
+}
+
+void
+FW::SpacePointFinder::storeSpacePoint(
+	const Acts::Vector3D& spacePoint,
+	const CombinedHits&	  hits,
+	DetData& 			  stripClusters) const
+{
+    // Receive the identification of the digitized hits on the first surface
+	Identifier       id(hits.hitModule1->identifier());
+	Acts::GeometryID geoID(id.value());
+
+	// The covariance is currently set to 0.
+	Acts::ActsSymMatrixD<2> cov;
+	cov << 0., 0., 0., 0.;
+
+	// Get the local coordinates of the space point
+	Acts::Vector2D local;
+	hits.hitModule1->referenceSurface().globalToLocal(
+		spacePoint, {0., 0., 0.}, local);
+
+	// Build the space point
+	Acts::PlanarModuleCluster pCluster(
+		hits.hitModule1->referenceSurface(),
+		Identifier(geoID.value()),
+		std::move(cov),
+		local[0],
+		local[1],
+		std::move(hits.hitModule1->digitizationCells()),
+		{hits.hitModule1->truthVertices()});
+
+	// Insert into the cluster map
+	FW::Data::insert(stripClusters,
+					 geoID.value(Acts::GeometryID::volume_mask),
+					 geoID.value(Acts::GeometryID::layer_mask),
+					 geoID.value(Acts::GeometryID::sensitive_mask),
+					 std::move(pCluster));
+}
+
 FW::DetectorData<geo_id_value, Acts::PlanarModuleCluster>
 FW::SpacePointFinder::calculateSpacePoints(
     std::vector<std::vector<CombinedHits>>& allCombHits,
@@ -291,15 +398,13 @@ FW::SpacePointFinder::calculateSpacePoints(
   // Source of algorithm: Athena, SiSpacePointMakerTool::makeSCT_SpacePoint()
   // TODO: some stability part still missing
 
-  // Declare storage of the found space points
-  std::vector<Acts::Vector3D> spacePoints;
-
-  FW::DetectorData<geo_id_value, Acts::PlanarModuleCluster> stripClusters;
+  DetData stripClusters;
+  FW::SpacePointFinder::SpacePointParameters spaPoPa;
 
   // Walk over every found candidate pair
   for (const auto& layers : allCombHits)
     for (const auto& hits : layers) {
-      // Calculate the ends of the strip detector elements
+      // Calculate the ends of the SDEs
       const auto& ends1 = endsOfStrip(*(hits.hitModule1));
       const auto& ends2 = endsOfStrip(*(hits.hitModule2));
 
@@ -320,56 +425,40 @@ FW::SpacePointFinder::calculateSpacePoints(
       /// needs to be fulfilled. Inserting the first equation into this
       /// equation leads to the condition for m as given in the following
       /// algorithm and therefore to the calculation of x.
+      /// The same calculation can be repeated for y. Its corresponding parameter will be named n.
 
-      Acts::Vector3D q  = ends1.first - ends1.second;
-      Acts::Vector3D s  = ends1.first + ends1.second - 2 * m_cfg.vertex;
-      Acts::Vector3D r  = ends2.first - ends2.second;
-      Acts::Vector3D t  = ends2.first + ends2.second - 2 * m_cfg.vertex;
-      Acts::Vector3D rt = r.cross(t);
-      double         m  = -s.dot(rt) / q.dot(rt);
-
-      // Check if hit is on strip
-      if (fabs(m) > 1) {
-        ACTS_INFO("Unable to combine hits with vertex (" << m_cfg.vertex[0]
-                                                         << ", "
-                                                         << m_cfg.vertex[1]
-                                                         << ", "
-                                                         << m_cfg.vertex[2]
-                                                         << ")");
-      } else {
-        // Store the space point
-        spacePoints.push_back(0.5 * (ends1.first + ends1.second + m * q));
-
-        // Receive the identification of the digitized hits on the first surface
-        Identifier       id(hits.hitModule1->identifier());
-        Acts::GeometryID geoID(id.value());
-
-        // The covariance is currently set to 0.
-        Acts::ActsSymMatrixD<2> cov;
-        cov << 0., 0., 0., 0.;
-
-        // Get the local coordinates of the space point
-        Acts::Vector2D local;
-        hits.hitModule1->referenceSurface().globalToLocal(
-            spacePoints.back(), {0., 0., 0.}, local);
-
-        // Build the space point
-        Acts::PlanarModuleCluster pCluster(
-            hits.hitModule1->referenceSurface(),
-            Identifier(geoID.value()),
-            std::move(cov),
-            local[0],
-            local[1],
-            std::move(hits.hitModule1->digitizationCells()),
-            {hits.hitModule1->truthVertices()});
-
-        // Insert into the cluster map
-        FW::Data::insert(stripClusters,
-                         geoID.value(Acts::GeometryID::volume_mask),
-                         geoID.value(Acts::GeometryID::layer_mask),
-                         geoID.value(Acts::GeometryID::sensitive_mask),
-                         std::move(pCluster));
-      }
+	  spaPoPa.reset();
+      spaPoPa.q  = ends1.first - ends1.second;
+      spaPoPa.s  = ends1.first + ends1.second - 2 * m_cfg.vertex;
+      spaPoPa.r  = ends2.first - ends2.second;
+      spaPoPa.t  = ends2.first + ends2.second - 2 * m_cfg.vertex;
+      spaPoPa.qs = spaPoPa.q.cross(spaPoPa.s);
+      spaPoPa.rt = spaPoPa.r.cross(spaPoPa.t);
+      spaPoPa.m  = -spaPoPa.s.dot(spaPoPa.rt)
+						/ spaPoPa.q.dot(spaPoPa.rt);
+      
+      // Set the limit for the parameter
+      if(spaPoPa.limit == 1. && m_cfg.stripLengthTolerance != 0.)
+		spaPoPa.limit = 1. + m_cfg.stripLengthTolerance;
+      
+      // Check if m and n can be resolved in the interval (-1, 1)
+      if(fabs(spaPoPa.m) <= spaPoPa.limit 
+         && fabs(spaPoPa.n = -spaPoPa.t.dot(spaPoPa.qs) / spaPoPa.r.dot(spaPoPa.qs)) <= spaPoPa.limit)
+		// Store the space point
+		storeSpacePoint(
+				0.5 * (ends1.first + ends1.second + spaPoPa.m * spaPoPa.q),
+				hits, 
+				stripClusters);
+	  else
+	  /// If this point is reached then it was not possible to resolve both points such that they are on their SDEs
+	  /// The following code treats a possible recovery of points resolved slightly outside of the SDE.
+	  /// @note This procedure is an indirect variation of the vertex position.
+		  // Check if a recovery the point(s) and store them if successful
+		  if(recoverSpacePoint(spaPoPa))
+			storeSpacePoint(
+				0.5 * (ends1.first + ends1.second + spaPoPa.m * spaPoPa.q),
+				hits, 
+				stripClusters);
     }
   // Return the resolved hits
   return stripClusters;
@@ -384,7 +473,7 @@ FW::SpacePointFinder::execute(AlgorithmContext ctx) const
   // Acts::PlanarModuleCluster>
   const DetData*                                               detData;
   std::vector<std::vector<FW::SpacePointFinder::CombinedHits>> allCombHits;
-  FW::DetectorData<geo_id_value, Acts::PlanarModuleCluster> stripClusters;
+  DetData stripClusters;
 
   // Receive all hits from the Whiteboard
   clusterReading(ctx, detData);
