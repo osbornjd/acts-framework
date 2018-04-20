413fe01d816e95c66141904eed568826abbc6775
Paul Gessinger <hello@paulgessinger.com>
2018-04-20 13:18:11 +0200

    fix format

--- Examples/MaterialMapping/src/MaterialMappingExample.cpp
+++ Examples/MaterialMapping/src/MaterialMappingExample.cpp
@@ -7,7 +7,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include <boost/program_options.hpp>
-#include <boost/program_options.hpp>
 #include "ACTFW/Extrapolation/ExtrapolationUtils.hpp"
 #include "ACTFW/Framework/Sequencer.hpp"
 #include "ACTFW/Framework/StandardOptions.hpp"
