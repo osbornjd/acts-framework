30861944a71fb141fb04fdd0064b401629d2c0e5
FabianKlimpel <fabian.klimpel@tum.de>
2018-05-03 13:18:53 +0200

    Format fixed

--- Plugins/HepMCReader/include/ACTFW/Plugins/HepMCReader/HepMCReader.hpp
+++ Plugins/HepMCReader/include/ACTFW/Plugins/HepMCReader/HepMCReader.hpp
@@ -12,122 +12,124 @@
 #include <fstream>
 #include <iostream>
 #include <mutex>
-#include "HepMC/GenEvent.h"
 #include "ACTS/EventData/ParticleDefinitions.hpp"
 #include "ACTS/Utilities/Units.hpp"
+#include "HepMC/GenEvent.h"
 
 namespace FW {
 
 /// @class HepMC3Reader
 ///
 /// An HepMC reader and converter to ACTS types.
-/// The HepMC::GenEvent usually stores a single event. This class is a child class from this class and extends the class by a persistent storage in ACTS data types.
+/// The HepMC::GenEvent usually stores a single event. This class is a child
+/// class from this class and extends the class by a persistent storage in ACTS
+/// data types.
 ///
 class HepMCReader : public HepMC::GenEvent
 {
 public:
- //TODO: writer for the complete file
+  // TODO: writer for the complete file
 
- /// @brief This structure stores general information about an event
-    struct EventHead
+  /// @brief This structure stores general information about an event
+  struct EventHead
   {
-	   // Index of the event
-	int eventNumber;
-	// Used unit of momentum (MeV or GeV)
-	double unitMomentum;
-	// Used unit of length (mm or cm)
-	double unitLength;
-	// Storage of the cross section (first) and its uncertainty (second)
-	std::pair<double, double> crossSection;
-	// Beam particles. Both are 0 if not set.
-	std::pair<pdg_type, pdg_type> beamParticles = std::make_pair(0, 0);
-	// Used weights of the event 
-	HepMC::WeightContainer* weights;
+    // Index of the event
+    int eventNumber;
+    // Used unit of momentum (MeV or GeV)
+    double unitMomentum;
+    // Used unit of length (mm or cm)
+    double unitLength;
+    // Storage of the cross section (first) and its uncertainty (second)
+    std::pair<double, double> crossSection;
+    // Beam particles. Both are 0 if not set.
+    std::pair<pdg_type, pdg_type> beamParticles = std::make_pair(0, 0);
+    // Used weights of the event
+    HepMC::WeightContainer* weights;
   };
-  
-    /// @brief This structure stores all information about the event
+
+  /// @brief This structure stores all information about the event
   struct EventStore
   {
-	  	   //TODO: heavy ions are not recorded yet
-	   // Storage of general information
-		EventHead evtHead;
-			// Storage of every vertex and particles
-		std::vector<Acts::ProcessVertex> vertices;
+    // TODO: heavy ions are not recorded yet
+    // Storage of general information
+    EventHead evtHead;
+    // Storage of every vertex and particles
+    std::vector<Acts::ProcessVertex> vertices;
   };
-  
+
   /// @brief Reader of a single event
   /// @param input stream to .hepmc file
-  void 
+  void
   readEvt(std::istream& is);
-  
-    /// @brief Reader of all events in a single file
-    /// @param input stream to .hepmc file
-void 
-readFile(std::istream& is);
 
-	/// @brief Getter of a single event
-	/// @param index index of the event
-	/// @return structure that contains all event data
-	const EventStore&
-	event(unsigned int index);
-	
-	/// @brief Getter of all events
-	/// @return list of all event data
-	const std::vector<EventStore>&
-	allEvents();
-	
-	/// @brief Getter of the head of a single event
-	/// @param index index of the event
-	/// @return head of the corresponding event
-	const EventHead&
-	eventHead(unsigned int index);
-	
-	/// @brief Getter of the head of all events
-	/// @return list of heads of all events 
-	const std::vector<EventHead>
-	allEventHeads();
-	
-	/// @brief Getter of a single vertex in an event
-	/// @param indexEvent index of the event
-	/// @param indexVertex index of the vertex in this event
-	/// @return vertex in the event
+  /// @brief Reader of all events in a single file
+  /// @param input stream to .hepmc file
+  void
+  readFile(std::istream& is);
+
+  /// @brief Getter of a single event
+  /// @param index index of the event
+  /// @return structure that contains all event data
+  const EventStore&
+  event(unsigned int index);
+
+  /// @brief Getter of all events
+  /// @return list of all event data
+  const std::vector<EventStore>&
+  allEvents();
+
+  /// @brief Getter of the head of a single event
+  /// @param index index of the event
+  /// @return head of the corresponding event
+  const EventHead&
+  eventHead(unsigned int index);
+
+  /// @brief Getter of the head of all events
+  /// @return list of heads of all events
+  const std::vector<EventHead>
+  allEventHeads();
+
+  /// @brief Getter of a single vertex in an event
+  /// @param indexEvent index of the event
+  /// @param indexVertex index of the vertex in this event
+  /// @return vertex in the event
   const Acts::ProcessVertex&
   vertex(unsigned int indexEvent, unsigned int indexVertex);
-  
+
   /// @brief Getter of all vertices in an event
   /// @param indexEvent index of the event
   /// @return list of all vertices in the event
   const std::vector<Acts::ProcessVertex>&
   vertices(unsigned int indexEvent);
-  
+
   /// @brief Getter of all vertices recorded
   /// @return list of all vertices recorded
   const std::vector<Acts::ProcessVertex>
   vertices();
 
 private:
-	// Storage of all events
-	std::vector<EventStore> events;
-	
-	/// @brief Storage of a read event
-	void 
-	storeEvent();
-	
-	/// @brief Creates and returns the head information about an event
-	/// @return head of the event
-	EventHead 
-	initHead();
-	
-	/// @brief Creates and returns the vertices in an event
-	/// @return list of vertices in an event
-	std::vector<Acts::ProcessVertex> 
-	storeEventBody();
-	
-	/// @brief Creates and returns a particle in an event
-	/// @param it iterator that points to a particle in an event
-	/// @return particle in an event
-	Acts::ParticleProperties 
-	buildParticle(HepMC::GenVertex::particles_in_const_iterator it);
-}; 
-} // FW
+  // Storage of all events
+  std::vector<EventStore> events;
+
+  /// @brief Storage of a read event
+  void
+  storeEvent();
+
+  /// @brief Creates and returns the head information about an event
+  /// @return head of the event
+  EventHead
+  initHead();
+
+  /// @brief Creates and returns the vertices in an event
+  /// @return list of vertices in an event
+  std::vector<Acts::ProcessVertex>
+  storeEventBody();
+
+  /// @brief Creates and returns a particle in an event
+  /// @param it iterator that points to a particle in an event
+  /// @return particle in an event
+  Acts::ParticleProperties
+  buildParticle(HepMC::GenVertex::particles_in_const_iterator it);
+};
+}  // FW
 #endif  // ACTFW_PLUGINS_HEPMCREADER_H
--- Plugins/HepMCReader/src/HepMC3Reader.cpp
+++ Plugins/HepMCReader/src/HepMC3Reader.cpp
@@ -7,159 +7,178 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "ACTFW/Plugins/HepMCReader/HepMC3Reader.hpp"
-#include "HepMC/ReaderAsciiHepMC2.h"
-#include "HepMC/ReaderAscii.h"
-#include "HepMC/GenParticle.h"
 #include <ios>
 #include <iostream>
 #include <stdexcept>
+#include "HepMC/GenParticle.h"
+#include "HepMC/ReaderAscii.h"
+#include "HepMC/ReaderAsciiHepMC2.h"
 
 FW::HepMC3Reader::HepMC3Reader(const std::string& filename, const int version)
 {
-	// Check if version should be 2.x or 3.x
-	assert(version != 2 && version != 3);
-	// Construct the reader depending on the version
-	switch(version)
-	{
-		case(2) : reader = new HepMC::ReaderAsciiHepMC2(filename);
-				  break;
-		case(3) : reader = new HepMC::ReaderAscii(filename);
-	}
+  // Check if version should be 2.x or 3.x
+  assert(version != 2 && version != 3);
+  // Construct the reader depending on the version
+  switch (version) {
+  case (2):
+    reader = new HepMC::ReaderAsciiHepMC2(filename);
+    break;
+  case (3):
+    reader = new HepMC::ReaderAscii(filename);
+  }
 }
 
-bool 
-FW::HepMC3Reader::readEvt() 
+bool
+FW::HepMC3Reader::readEvt()
 {
-	// Read an event
-	bool success = reader->read_event(*this);
-	// Store the data of the event
-	if(success) storeEvent();
-	// Return if the reading was successful
-	return success;
+  // Read an event
+  bool success = reader->read_event(*this);
+  // Store the data of the event
+  if (success) storeEvent();
+  // Return if the reading was successful
+  return success;
 }
 
-void 
+void
 FW::HepMC3Reader::readFile()
 {
-	// Loop through the whole file and store the data
-	while(readEvt()) {}
+  // Loop through the whole file and store the data
+  while (readEvt()) {
+  }
 }
 
 Acts::ParticleProperties
-FW::HepMC3Reader::buildParticle(HepMC::GenVertex::particles_in_const_iterator it)
+FW::HepMC3Reader::buildParticle(
+    HepMC::GenVertex::particles_in_const_iterator it)
 {
-	// Extract the particles information from the iterator and store it in an ACTS particle object
-	return std::move(Acts::ParticleProperties({(*it)->momentum().x(), (*it)->momentum().y(), (*it)->momentum().z()}, 
-											  (*it)->generated_mass(),
-											  0, //TODO: charge is not in genparticle -> use pid?
-											  (*it)->pdg_id(),
-											  (*it)->id()));
+  // Extract the particles information from the iterator and store it in an ACTS
+  // particle object
+  return std::move(Acts::ParticleProperties(
+      {(*it)->momentum().x(), (*it)->momentum().y(), (*it)->momentum().z()},
+      (*it)->generated_mass(),
+      0,  // TODO: charge is not in genparticle -> use pid?
+      (*it)->pdg_id(),
+      (*it)->id()));
 }
 
-void 
-FW::HepMC3Reader::storeEvent() 
+void
+FW::HepMC3Reader::storeEvent()
 {
-	// Store the head data and the vertices 
-	EventStore evtStore;
-	evtStore.evtHead = initHead();
-	evtStore.vertices = storeEventBody();	
-	// Store the event persistent
-	events.push_back(std::move(evtStore));
+  // Store the head data and the vertices
+  EventStore evtStore;
+  evtStore.evtHead  = initHead();
+  evtStore.vertices = storeEventBody();
+  // Store the event persistent
+  events.push_back(std::move(evtStore));
 }
 
 FW::HepMC3Reader::EventHead
 FW::HepMC3Reader::initHead()
 {
-	// Extract the information for the member variables of HepMC3Reader::EventHead from HepMC::GenEvent and store these
-	FW::HepMC3Reader::EventHead evtHead;
-	evtHead.eventNumber = this->event_number();
-	evtHead.unitMomentum = (momentum_unit() == HepMC::Units::MomentumUnit::MEV ? Acts::units::_MeV : Acts::units::_GeV);
-	evtHead.unitLength = (length_unit() == HepMC::Units::LengthUnit::MM ? Acts::units::_mm : Acts::units::_cm);
-	evtHead.crossSection = std::make_pair(cross_section()->cross_section, cross_section()->cross_section_error);
-	if(beam_particles().first && beam_particles().second) evtHead.beamParticles = std::make_pair(beam_particles().first->pid(), beam_particles().second->pid());
-	evtHead.weights = &(weights());
-	evtHead.posVertex = {event_pos().x(), event_pos().y(), event_pos().z()};
-	evtHead.timeVertex = event_pos().t();
-	// Return the HepMC3Reader::EventHead with its data
-	return std::move(evtHead);
+  // Extract the information for the member variables of HepMC3Reader::EventHead
+  // from HepMC::GenEvent and store these
+  FW::HepMC3Reader::EventHead evtHead;
+  evtHead.eventNumber  = this->event_number();
+  evtHead.unitMomentum = (momentum_unit() == HepMC::Units::MomentumUnit::MEV
+                              ? Acts::units::_MeV
+                              : Acts::units::_GeV);
+  evtHead.unitLength
+      = (length_unit() == HepMC::Units::LengthUnit::MM ? Acts::units::_mm
+                                                       : Acts::units::_cm);
+  evtHead.crossSection = std::make_pair(cross_section()->cross_section,
+                                        cross_section()->cross_section_error);
+  if (beam_particles().first && beam_particles().second)
+    evtHead.beamParticles = std::make_pair(beam_particles().first->pid(),
+                                           beam_particles().second->pid());
+  evtHead.weights    = &(weights());
+  evtHead.posVertex  = {event_pos().x(), event_pos().y(), event_pos().z()};
+  evtHead.timeVertex = event_pos().t();
+  // Return the HepMC3Reader::EventHead with its data
+  return std::move(evtHead);
 }
 
 std::vector<Acts::ProcessVertex>
 FW::HepMC3Reader::storeEventBody()
 {
-	std::vector<Acts::ProcessVertex> vertices;
-	// Loop through every vertex in the event
-	for (HepMC::GenEvent::vertex_const_iterator vtx = this->vertices_begin(); vtx != this->vertices_end(); ++vtx ) 
-	{		
-	    std::vector<Acts::ParticleProperties> particlesIn, particlesOut;
-	    // Loop through every particle that entered the vertex and store it
-	    for(HepMC::GenVertex::particles_in_const_iterator pIn = (*vtx)->particles_in_const_begin(); pIn != (*vtx)->particles_in_const_end(); pIn++)
-			particlesIn.push_back(std::move(buildParticle(pIn)));
-		// Loop through every particle that exited the vertex and store it
-		for(HepMC::GenVertex::particles_out_const_iterator pOut = (*vtx)->particles_out_const_begin(); pOut != (*vtx)->particles_out_const_end(); pOut++)
-			particlesOut.push_back(std::move(buildParticle(pOut)));
-		
-		// Create a vertex and store it
-		Acts::ProcessVertex vertex({(*vtx)->position().x(), (*vtx)->position().y(), (*vtx)->position().z()},
-										 (*vtx)->position().t(),
-										 0, //TODO: what does process_type?
-										 particlesIn,
-										 particlesOut);
-		vertices.push_back(std::move(vertex));
-	}
-	// Return the list of all vertices in the event
-	return std::move(vertices);
+  std::vector<Acts::ProcessVertex> vertices;
+  // Loop through every vertex in the event
+  for (HepMC::GenEvent::vertex_const_iterator vtx = this->vertices_begin();
+       vtx != this->vertices_end();
+       ++vtx) {
+    std::vector<Acts::ParticleProperties> particlesIn, particlesOut;
+    // Loop through every particle that entered the vertex and store it
+    for (HepMC::GenVertex::particles_in_const_iterator pIn
+         = (*vtx)->particles_in_const_begin();
+         pIn != (*vtx)->particles_in_const_end();
+         pIn++)
+      particlesIn.push_back(std::move(buildParticle(pIn)));
+    // Loop through every particle that exited the vertex and store it
+    for (HepMC::GenVertex::particles_out_const_iterator pOut
+         = (*vtx)->particles_out_const_begin();
+         pOut != (*vtx)->particles_out_const_end();
+         pOut++)
+      particlesOut.push_back(std::move(buildParticle(pOut)));
+
+    // Create a vertex and store it
+    Acts::ProcessVertex vertex({(*vtx)->position().x(),
+                                (*vtx)->position().y(),
+                                (*vtx)->position().z()},
+                               (*vtx)->position().t(),
+                               0,  // TODO: what does process_type?
+                               particlesIn,
+                               particlesOut);
+    vertices.push_back(std::move(vertex));
+  }
+  // Return the list of all vertices in the event
+  return std::move(vertices);
 }
 
 const FW::HepMC3Reader::EventStore&
 FW::HepMC3Reader::event(unsigned int index)
 {
-	return events[index];
+  return events[index];
 }
 
 const std::vector<FW::HepMC3Reader::EventStore>&
 FW::HepMC3Reader::allEvents()
 {
-	return events;
+  return events;
 }
 
 const FW::HepMC3Reader::EventHead&
 FW::HepMC3Reader::eventHead(unsigned int index)
 {
-	return events[index].evtHead;
+  return events[index].evtHead;
 }
 
 const std::vector<FW::HepMC3Reader::EventHead>
 FW::HepMC3Reader::allEventHeads()
 {
-	std::vector<FW::HepMC3Reader::EventHead> heads;
-	// Loop through all events and extract the head of the event
-	for(auto& event : events)
-		heads.push_back(event.evtHead);
-	return heads;
+  std::vector<FW::HepMC3Reader::EventHead> heads;
+  // Loop through all events and extract the head of the event
+  for (auto& event : events) heads.push_back(event.evtHead);
+  return heads;
 }
 
 const Acts::ProcessVertex&
 FW::HepMC3Reader::vertex(unsigned int indexEvent, unsigned int indexVertex)
 {
-	return events[indexEvent].vertices[indexVertex];
+  return events[indexEvent].vertices[indexVertex];
 }
 
 const std::vector<Acts::ProcessVertex>&
 FW::HepMC3Reader::vertices(unsigned int indexEvent)
 {
-	return events[indexEvent].vertices;
+  return events[indexEvent].vertices;
 }
 
 const std::vector<Acts::ProcessVertex>
 FW::HepMC3Reader::vertices()
 {
-	std::vector<Acts::ProcessVertex> vertices;
-	// Loop through all events
-	for(auto& event : events)
-		// Loop through all vertices and store them
-		for(auto& vertex : event.vertices)
-			vertices.push_back(vertex);
-	return vertices;
+  std::vector<Acts::ProcessVertex> vertices;
+  // Loop through all events
+  for (auto& event : events)
+    // Loop through all vertices and store them
+    for (auto& vertex : event.vertices) vertices.push_back(vertex);
+  return vertices;
 }
-  
