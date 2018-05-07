40846832df5095687036b426a34f90dcf193a23b
FabianKlimpel <fabian.klimpel@tum.de>
2018-05-07 13:10:53 +0200

    Format fixed

--- Examples/HepMCExample/src/HepMC3Example.cpp
+++ Examples/HepMCExample/src/HepMC3Example.cpp
@@ -7,36 +7,39 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include <boost/program_options.hpp>
+#include <fstream>
 #include <memory>
+#include "ACTFW/Plugins/HepMC/HepMC3Run.hpp"
 #include "HepMC/Print.h"
 #include "HepMC/ReaderAscii.h"
-#include <fstream>
 #include "HepPID/ParticleName.hh"
-#include "ACTFW/Plugins/HepMC/HepMC3Run.hpp"
 
 int
 main(int argc, char* argv[])
 {
   FW::HepMC3Run hepmc3run("example.hepmc3");
   hepmc3run.readEvent();
-  
-  const std::vector<std::shared_ptr<Acts::ProcessVertex>> vertices2 = hepmc3run.event(0)->vertices();
+
+  const std::vector<std::shared_ptr<Acts::ProcessVertex>> vertices2
+      = hepmc3run.event(0)->vertices();
   std::cout << "Number of vertices: " << vertices2.size() << std::endl;
-  const std::vector<Acts::ParticleProperties> incoming2 = vertices2[0]->incomingParticles();
+  const std::vector<Acts::ParticleProperties> incoming2
+      = vertices2[0]->incomingParticles();
   std::cout << "Incoming particles: " << incoming2.size() << std::endl;
-  const std::vector<Acts::ParticleProperties> outgoing2 = vertices2[0]->outgoingParticles();
+  const std::vector<Acts::ParticleProperties> outgoing2
+      = vertices2[0]->outgoingParticles();
   std::cout << "Outgoing particles: " << outgoing2.size() << std::endl;
   std::cout << "First vertex: ";
-  for(Acts::ParticleProperties in : incoming2)
-	std::cout << HepPID::particleName(in.pdgID()) << " ";
+  for (Acts::ParticleProperties in : incoming2)
+    std::cout << HepPID::particleName(in.pdgID()) << " ";
   std::cout << " -> ";
-  for(Acts::ParticleProperties out : outgoing2)
-	std::cout << HepPID::particleName(out.pdgID()) << " ";
+  for (Acts::ParticleProperties out : outgoing2)
+    std::cout << HepPID::particleName(out.pdgID()) << " ";
   std::cout << std::endl;
   std::cout << "Beams: ";
-  std::vector<std::shared_ptr<Acts::ParticleProperties>> beams = hepmc3run.event(0)->beams();
-  for(auto& beam : beams)
-	std::cout << HepPID::particleName(beam->pdgID()) << " ";
+  std::vector<std::shared_ptr<Acts::ParticleProperties>> beams
+      = hepmc3run.event(0)->beams();
+  for (auto& beam : beams)
+    std::cout << HepPID::particleName(beam->pdgID()) << " ";
   std::cout << std::endl;
-  
 }
