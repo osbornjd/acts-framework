f4b82de875f47e000f48e34403166390cfa57b32
Valentin Volkl <valentin.volkl@cern.ch>
2018-10-11 17:13:28 +0200

    clang-format

--- Plugins/Root/include/ACTFW/Plugins/Root/RootPlanarClusterWriter.hpp
+++ Plugins/Root/include/ACTFW/Plugins/Root/RootPlanarClusterWriter.hpp
@@ -93,12 +93,13 @@ namespace Root {
     std::vector<float> m_cell_data;   ///< local cell position y
 
     // (optional) the truth position
-    std::vector<float> m_t_gx;       ///< truth position global x
-    std::vector<float> m_t_gy;       ///< truth position global y
-    std::vector<float> m_t_gz;       ///< truth position global z
-    std::vector<float> m_t_lx;       ///< truth position local x
-    std::vector<float> m_t_ly;       ///< truth position local y
-    std::vector<unsigned long>   m_t_barcode;  ///< associated truth particle barcode
+    std::vector<float> m_t_gx;  ///< truth position global x
+    std::vector<float> m_t_gy;  ///< truth position global y
+    std::vector<float> m_t_gz;  ///< truth position global z
+    std::vector<float> m_t_lx;  ///< truth position local x
+    std::vector<float> m_t_ly;  ///< truth position local y
+    std::vector<unsigned long>
+        m_t_barcode;  ///< associated truth particle barcode
   };
 
 }  // namespace Root
