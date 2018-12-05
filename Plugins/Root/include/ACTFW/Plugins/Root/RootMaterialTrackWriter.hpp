26b13baa04f18d132880a75afcbf7a8a914618be
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2018-12-05 16:03:55 +0100

    re-established RecordedMaterialTrack writer

--- Plugins/Root/include/ACTFW/Plugins/Root/RootMaterialTrackWriter.hpp
+++ Plugins/Root/include/ACTFW/Plugins/Root/RootMaterialTrackWriter.hpp
@@ -6,15 +6,13 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#ifndef ACTFW_PLUGINS_ROOT_RootMaterialTrackWriter_H
-#define ACTFW_PLUGINS_ROOT_RootMaterialTrackWriter_H
+#pragma once
 
 #include <mutex>
 #include "ACTFW/Framework/IService.hpp"
 #include "ACTFW/Framework/ProcessCode.hpp"
-#include "ACTFW/Writers/IWriterT.hpp"
-#include "Acts/Plugins/MaterialMapping/MaterialTrack.hpp"
-#include "Acts/Utilities/Definitions.hpp"
+#include "ACTFW/Framework/WriterT.hpp"
+#include "Acts/Plugins/MaterialMapping/RecordedMaterialTrack.hpp"
 #include "Acts/Utilities/Logger.hpp"
 #include "TTree.h"
 
@@ -24,83 +22,84 @@ namespace Root {
 
   /// @class RootMaterialTrackWriter
   ///
-  /// @brief Writes out MaterialTrack entities from a root file
+  /// @brief Writes out MaterialTrack collections from a root file
   ///
   /// This service is the root implementation of the IWriterT.
   /// It writes out a MaterialTrack which is usually generated from
   /// Geant4 material mapping
-
-  class RootMaterialTrackWriter : public FW::IWriterT<Acts::MaterialTrack>
+  class RootMaterialTrackWriter
+      : public WriterT<std::vector<Acts::RecordedMaterialTrack>>
   {
-
   public:
-    /// @class Config
-    /// Configuration of the Writer
-    class Config
-    {
-    public:
-      /// The name of the output tree
-      std::string treeName;
-      /// The name of the output file
-      std::string fileName;
-      /// The default logger
-      std::shared_ptr<const Acts::Logger> logger;
-      /// The name of the service
-      std::string name;
+    using Base = WriterT<std::vector<Acts::RecordedMaterialTrack>>;
 
-      Config(const std::string&   lname = "MaterialWriter",
-             Acts::Logging::Level lvl   = Acts::Logging::INFO)
-        : logger(Acts::getDefaultLogger(lname, lvl)), name(lname)
-      {
-      }
+    /// @brief The nested configuration struct
+    struct Config
+    {
+      std::string collection = "";          ///< particle collection to write
+      std::string filePath   = "";          ///< path of the output file
+      std::string fileMode   = "RECREATE";  ///< file access mode
+      std::string treeName   = "materialtracks";  ///< name of the output tree
+      TFile*      rootFile   = nullptr;           ///< common root file
     };
 
-    /// Constructor
+    /// Constructor with
+    /// @param cfg configuration struct
+    /// @param output logging level
+    RootMaterialTrackWriter(const Config&        cfg,
+                            Acts::Logging::Level level = Acts::Logging::INFO);
+
     RootMaterialTrackWriter(const Config& cfg);
 
     /// Virtual destructor
     ~RootMaterialTrackWriter() override;
 
-    /// Framework name() method
-    std::string
-    name() const final override;
-
     /// Framework intialize method
     FW::ProcessCode
     endRun() final override;
 
-    /// Interface method which writes out the MaterialTrack entities
-    /// @param mtrecord is the material track record to be written out
-    FW::ProcessCode
-    write(const Acts::MaterialTrack& mtrecord) final override;
+  protected:
+    // This implementation holds the actual writing method
+    /// and is called by the WriterT<>::write interface
+    ///
+    /// @param ctx The Algorithm context with per event information
+    /// @param clusters is the data to be written out
+    ProcessCode
+    writeT(const AlgorithmContext&                         ctx,
+           const std::vector<Acts::RecordedMaterialTrack>& materialtracks)
+        final override;
 
   private:
     /// The config class
     Config m_cfg;
     /// mutex used to protect multi-threaded writes
-    std::mutex m_write_mutex;
+    std::mutex m_writeMutex;
     /// The output file name
     TFile* m_outputFile;
     /// The output tree name
     TTree* m_outputTree;
-    // The MaterialTrack to be written out
-    Acts::MaterialTrack m_trackRecord;
 
-    /// Private access to the logging instance
-    const Acts::Logger&
-    logger() const
-    {
-      return *m_cfg.logger;
-    }
+    float m_v_x;    ///< start global x
+    float m_v_y;    ///< start global y
+    float m_v_z;    ///< start global z
+    float m_v_dx;   ///< start global direction x
+    float m_v_dy;   ///< start global direction y
+    float m_v_dz;   ///< start global direction z
+    float m_v_phi;  ///< start phi direction
+    float m_v_eta;  ///< start eta direction
+    float m_tX0;    ///< thickness in X0/L0
+    float m_tL0;    ///< thickness in X0/L0
+
+    std::vector<float> m_step_x;       ///< step x position
+    std::vector<float> m_step_y;       ///< step y position
+    std::vector<float> m_step_z;       ///< step z position
+    std::vector<float> m_step_length;  ///< step length
+    std::vector<float> m_step_X0;      ///< step material x0
+    std::vector<float> m_step_L0;      ///< step material l0
+    std::vector<float> m_step_A;       ///< step material A
+    std::vector<float> m_step_Z;       ///< step material Z
+    std::vector<float> m_step_rho;     ///< step material rho
   };
-
-  inline std::string
-  RootMaterialTrackWriter::name() const
-  {
-    return m_cfg.name;
-  }
 
 }  // namespace Root
 }  // namespace FW
-
-#endif  // ACTFW_PLUGINS_ROOT_RootMaterialTrackWriter_H
--- Plugins/Root/src/RootMaterialTrackWriter.cpp
+++ Plugins/Root/src/RootMaterialTrackWriter.cpp
@@ -7,7 +7,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "ACTFW/Plugins/Root/RootMaterialTrackWriter.hpp"
-#include "Acts/Plugins/MaterialMapping/MaterialStep.hpp"
 
 #include <ios>
 #include <iostream>
@@ -15,36 +14,56 @@
 
 #include "TFile.h"
 
+#include "Acts/Utilities/Helpers.hpp"
+
+using Acts::VectorHelpers::eta;
+using Acts::VectorHelpers::phi;
+using Acts::VectorHelpers::perp;
+
 FW::Root::RootMaterialTrackWriter::RootMaterialTrackWriter(
-    const FW::Root::RootMaterialTrackWriter::Config& cfg)
-  : FW::IWriterT<Acts::MaterialTrack>()
+    const FW::Root::RootMaterialTrackWriter::Config& cfg,
+    Acts::Logging::Level                             level)
+  : Base(cfg.collection, "RootMaterialTrackWriter", level)
   , m_cfg(cfg)
-  , m_outputFile(nullptr)
-  , m_outputTree(nullptr)
-  , m_trackRecord()
+  , m_outputFile(cfg.rootFile)
 {
   // An input collection name and tree name must be specified
-  if (m_cfg.treeName.empty()) {
+  if (m_cfg.collection.empty()) {
+    throw std::invalid_argument("Missing input collection");
+  } else if (m_cfg.treeName.empty()) {
     throw std::invalid_argument("Missing tree name");
-  } else if (m_cfg.fileName.empty()) {
-    throw std::invalid_argument("Missing file name");
-  } else if (!m_cfg.logger) {
-    throw std::invalid_argument("Missing logger");
-  } else if (m_cfg.name.empty()) {
-    throw std::invalid_argument("Missing service name");
   }
 
   // Setup ROOT I/O
-  m_outputFile = TFile::Open(m_cfg.fileName.c_str(), "recreate");
-  if (!m_outputFile) {
-    throw std::ios_base::failure("Could not open '" + m_cfg.fileName);
+  if (m_outputFile == nullptr) {
+    m_outputFile = TFile::Open(m_cfg.filePath.c_str(), m_cfg.fileMode.c_str());
+    if (m_outputFile == nullptr) {
+      throw std::ios_base::failure("Could not open '" + m_cfg.filePath);
+    }
   }
   m_outputFile->cd();
-  m_outputTree = new TTree(m_cfg.treeName.c_str(), m_cfg.treeName.c_str());
-  if (!m_outputTree) throw std::bad_alloc();
+  m_outputTree
+      = new TTree(m_cfg.treeName.c_str(), "TTree from RootMaterialTrackWriter");
+  if (m_outputTree == nullptr) throw std::bad_alloc();
 
-  // Create a branch with the MaterialTrack entities
-  m_outputTree->Branch("MaterialTrack", &m_trackRecord);
+  // Set the branches
+  m_outputTree->Branch("v_x", &m_v_x);
+  m_outputTree->Branch("v_y", &m_v_y);
+  m_outputTree->Branch("v_z", &m_v_z);
+  m_outputTree->Branch("v_dx", &m_v_dx);
+  m_outputTree->Branch("v_dy", &m_v_dy);
+  m_outputTree->Branch("v_dz", &m_v_dz);
+  m_outputTree->Branch("v_phi", &m_v_phi);
+  m_outputTree->Branch("v_eta", &m_v_eta);
+  m_outputTree->Branch("mat_x", &m_step_x);
+  m_outputTree->Branch("mat_x", &m_step_y);
+  m_outputTree->Branch("mat_x", &m_step_z);
+  m_outputTree->Branch("mat_step_length", &m_step_length);
+  m_outputTree->Branch("mat_X0", &m_step_X0);
+  m_outputTree->Branch("mat_L0", &m_step_L0);
+  m_outputTree->Branch("mat_A", &m_step_A);
+  m_outputTree->Branch("mat_Z", &m_step_Z);
+  m_outputTree->Branch("mat_rho", &m_step_rho);
 }
 
 FW::Root::RootMaterialTrackWriter::~RootMaterialTrackWriter()
@@ -56,24 +75,79 @@ FW::ProcessCode
 FW::Root::RootMaterialTrackWriter::endRun()
 {
   // write the tree and close the file
-  ACTS_INFO("Writing ROOT output File : " << m_cfg.fileName);
+  ACTS_INFO("Writing ROOT output File : " << m_cfg.filePath);
   m_outputFile->cd();
   m_outputTree->Write();
   return FW::ProcessCode::SUCCESS;
 }
 
 FW::ProcessCode
-FW::Root::RootMaterialTrackWriter::write(const Acts::MaterialTrack& mtrecord)
+FW::Root::RootMaterialTrackWriter::writeT(
+    const AlgorithmContext&                         ctx,
+    const std::vector<Acts::RecordedMaterialTrack>& materialTracks)
 {
+  // Exclusive access to the tree while writing
+  std::lock_guard<std::mutex> lock(m_writeMutex);
 
-  // lock the mutex
-  std::lock_guard<std::mutex> lock(m_write_mutex);
+  // loop over the material tracks and write them out
+  for (auto& mtrack : materialTracks) {
 
-  // setting the parameters
-  m_trackRecord = mtrecord;
+    // clearing the vector first
+    m_step_x.clear();
+    m_step_y.clear();
+    m_step_z.clear();
+    m_step_length.clear();
+    m_step_X0.clear();
+    m_step_L0.clear();
+    m_step_A.clear();
+    m_step_Z.clear();
+    m_step_rho.clear();
 
-  // write to
-  m_outputTree->Fill();
+    // reserve the vector then
+    size_t msteps = mtrack.recordedMaterialProperties().size();
+    m_step_x.reserve(msteps);
+    m_step_y.reserve(msteps);
+    m_step_z.reserve(msteps);
+    m_step_length.reserve(msteps);
+    m_step_X0.reserve(msteps);
+    m_step_L0.reserve(msteps);
+    m_step_A.reserve(msteps);
+    m_step_Z.reserve(msteps);
+    m_step_rho.reserve(msteps);
+
+    // reset the global counter
+    m_tX0 = 0.;
+    m_tL0 = 0.;
+
+    // set the track information at vertex
+    m_v_x   = mtrack.position().x();
+    m_v_y   = mtrack.position().y();
+    m_v_z   = mtrack.position().z();
+    m_v_dx  = mtrack.direction().x();
+    m_v_dy  = mtrack.direction().y();
+    m_v_dz  = mtrack.direction().z();
+    m_v_phi = phi(mtrack.direction());
+    m_v_eta = eta(mtrack.direction());
+
+    // an now loop over the material
+    for (auto& mstep : mtrack.recordedMaterialProperties()) {
+      // the material step position information
+      m_step_x.push_back(mstep.second.x());
+      m_step_y.push_back(mstep.second.y());
+      m_step_z.push_back(mstep.second.z());
+      // the material information
+      auto mprops = mstep.first;
+      m_step_length.push_back(mprops.thickness());
+      m_step_X0.push_back(mprops.averageX0());
+      m_step_L0.push_back(mprops.averageL0());
+      m_step_A.push_back(mprops.averageA());
+      m_step_Z.push_back(mprops.averageZ());
+      m_step_rho.push_back(mprops.averageRho());
+    }
+
+    // write to
+    m_outputTree->Fill();
+  }
 
   // return success
   return FW::ProcessCode::SUCCESS;
