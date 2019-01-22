fa2a91738b2ce9c10ff69e57f0efe36163903278
msmk <msmk@cern.ch>
2019-01-22 14:51:40 +0100

    core: fix includes for options helpers

--- Core/include/ACTFW/Utilities/Options.hpp
+++ Core/include/ACTFW/Utilities/Options.hpp
@@ -8,38 +8,53 @@
 
 #pragma once
 
+#include <array>
+#include <ostream>
 #include <string>
+#include <vector>
 
 using read_series  = std::vector<int>;
 using read_range   = std::vector<double>;
 using read_strings = std::vector<std::string>;
 
 namespace std {
+namespace detail {
+  namespace {
 
-std::ostream&
+    /// Helper function to print multiple elements in a container
+    template <typename Iterator>
+    inline std::ostream&
+    printVector(Iterator      begin,
+                Iterator      end,
+                const char*   separator,
+                std::ostream& os)
+    {
+      for (auto it = begin; it != end; ++it) {
+        if (it != begin) { os << separator; }
+        os << *it;
+      }
+      return os;
+    }
+
+  }  // namespace
+}  // namespace detail
+
+inline std::ostream&
 operator<<(std::ostream& os, const read_series& vec)
 {
-  for (auto item : vec) {
-    os << item << " ";
-  }
-  return os;
+  return detail::printVector(vec.begin(), vec.end(), " ", os);
 }
 
-std::ostream&
+inline std::ostream&
 operator<<(std::ostream& os, const read_range& vec)
 {
-  for (auto item : vec) {
-    os << item << " ";
-  }
-  return os;
+  return detail::printVector(vec.begin(), vec.end(), " ", os);
 }
 
-std::ostream&
+inline std::ostream&
 operator<<(std::ostream& os, const read_strings& vec)
 {
-  for (auto item : vec) {
-    os << item << " ";
-  }
-  return os;
+  return detail::printVector(vec.begin(), vec.end(), " ", os);
 }
-}
+
+}  // namespace std
