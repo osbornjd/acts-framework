fe432ad5a5cc2bfd8e70c904abc3c8463555b0ab
Xiaocong Ai <xiaocong.ai@cern.ch>
2019-04-16 18:22:21 +0200

    Add a FittingAlgorithm: 1)create measurements using truth hits (smeared with gaussian) and feed them into a KalmanFitter instance; 2)store the fitted track, i.e. a few fittedStates into eventStore; Add a RootTrackWriter to write the track to a tree; Add the fitting example (nested in FatrasExample at the moment).

---
+++ Algorithms/Fitting/include/ACTFW/Fitting/FittingOptions.hpp
@@ -0,0 +1,71 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2018 Acts project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include <iostream>
+#include "ACTFW/Utilities/Options.hpp"
+#include "FittingAlgorithm.hpp"
+
+namespace po = boost::program_options;
+
+namespace FW {
+
+  namespace Options {
+
+    /// @brief read the Fitting options
+    ///
+    /// Adding Fitting specific options to the Options package
+    ///
+    /// @tparam aopt_t Type of the options object (API bound to boost)
+    ///
+    /// @param [in] opt_t The options object where the specific digitization
+    /// options are attached to
+    template <typename aopt_t>
+    void
+    addFittingOptions(aopt_t& opt)
+    {
+      opt.add_options()(
+                        "fatras-sim-particles",
+                        po::value<std::string>()->default_value("fatras-particles"),
+                        "The collection of simulated particles.")(
+                        "fatras-sim-hits",
+                        po::value<std::string>()->default_value("fatras-hits"),
+                        "The collection of simulated hits")(
+                        "fitted-tracks",
+                        po::value<std::string>()->default_value("fitted-tracks"),
+                        "The collection of output tracks");
+    }
+
+    /// @brief read the fitter specific options and return a Config file
+    ///
+    ///@tparam omap_t Type of the options map
+    ///@param vm the options map to be read out
+    template <typename AMAP,
+              typename kalman_Fitter_t>
+    typename FittingAlgorithm<kalman_Fitter_t>::Config
+    readFittingConfig(const AMAP& vm, kalman_Fitter_t& fitter)
+    {
+      // Create a config
+      typename FittingAlgorithm<kalman_Fitter_t>::Config
+        fittingConfig(std::move(fitter));
+
+      // set the collections
+      fittingConfig.simulatedHitCollection
+        = vm["fatras-sim-hits"].template as<std::string>();
+      fittingConfig.simulatedEventCollection
+        = vm["fatras-sim-particles"].template as<std::string>();
+      fittingConfig.trackCollection
+        = vm["fitted-tracks"].template as<std::string>();
+
+      // and return the config
+      return fittingConfig;
+    }
+
+  }  // namespace Options
+}  // namespace FW
---
+++ Examples/Fatras/src/detail/FittingBase.hpp
@@ -0,0 +1,151 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2018 Acts project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "ACTFW/Barcode/BarcodeSvc.hpp"
+#include "ACTFW/Fitting/FittingAlgorithm.hpp"
+#include "ACTFW/Fitting/FittingOptions.hpp"
+#include "ACTFW/Framework/Sequencer.hpp"
+#include "ACTFW/Plugins/BField/BFieldOptions.hpp"
+#include "ACTFW/Plugins/Root/RootTrackWriter.hpp"
+#include "ACTFW/Utilities/Paths.hpp"
+#include "Acts/Utilities/GeometryID.hpp"
+#include "Acts/Detector/TrackingGeometry.hpp"
+#include "Acts/Extrapolator/Navigator.hpp"
+#include "Acts/MagneticField/ConstantBField.hpp"
+#include "Acts/MagneticField/InterpolatedBFieldMap.hpp"
+#include "Acts/MagneticField/SharedBField.hpp"
+#include "Acts/Propagator/EigenStepper.hpp"
+#include "Acts/Propagator/Propagator.hpp"
+#include "Acts/Surfaces/Surface.hpp"
+#include "Acts/Fitter/GainMatrixSmoother.hpp"
+#include "Acts/Fitter/GainMatrixUpdator.hpp"
+#include "Acts/Fitter/KalmanFitter.hpp"
+
+
+/// @brief Fitting setup
+///
+/// @tparam bfield_t Type of the bfield for the fitting to be set up
+///
+/// @param fieldMap The field map for the fitting setup
+/// @param sequencer The framework sequencer
+/// @param vm The boost variable map to resolve
+/// @param tGeometry The TrackingGeometry for the tracking setup
+/// @param barcodesSvc The barcode service to be used for the fitting 
+template <typename bfield_t>
+void
+setupFittingAlgorithm(
+                      bfield_t                                      fieldMap,
+                      FW::Sequencer&                                sequencer,
+                      po::variables_map&                            vm,
+                      std::shared_ptr<const Acts::TrackingGeometry> tGeometry,
+                      std::shared_ptr<FW::BarcodeSvc>               barcodeSvc)
+{
+
+  // Read the log level
+  Acts::Logging::Level logLevel = FW::Options::readLogLevel(vm);
+
+  // Create a navigator for this tracking geometry
+  Acts::Navigator cNavigator(tGeometry);
+  cNavigator.resolvePassive   = false;
+  cNavigator.resolveMaterial  = true;
+  cNavigator.resolveSensitive = true;
+
+  using ChargedStepper    = Acts::EigenStepper<bfield_t>;
+  using ChargedPropagator = Acts::Propagator<ChargedStepper, Acts::Navigator>;
+
+  ChargedStepper    cStepper(std::move(fieldMap));
+  ChargedPropagator cPropagator(std::move(cStepper), std::move(cNavigator));
+
+  using Updator      = Acts::GainMatrixUpdator<Acts::BoundParameters>;
+  using Smoother     = Acts::GainMatrixSmoother<Acts::BoundParameters>;
+
+  using KalmanFitter = Acts::KalmanFitter<ChargedPropagator, Updator, Smoother>;
+
+  KalmanFitter kFitter(cPropagator);
+
+  using FittingAlgorithm = FW::FittingAlgorithm<KalmanFitter>;
+ 
+  typename FittingAlgorithm::Config fittingConfig = FW::Options::readFittingConfig<
+    po::variables_map, 
+    KalmanFitter>(vm, kFitter);
+  fittingConfig.simulatedHitCollection
+    = vm["fatras-sim-hits"].template as<std::string>();
+  fittingConfig.simulatedEventCollection
+    = vm["fatras-sim-particles"].template as<std::string>();
+  fittingConfig.trackCollection
+    = vm["fitted-tracks"].template as<std::string>();
+
+  // Finally the fitting algorithm
+  auto fittingAlgorithm
+    = std::make_shared<FittingAlgorithm>(fittingConfig, logLevel);
+
+  // Finalize the squencer setting and run
+  sequencer.appendEventAlgorithms({fittingAlgorithm});
+ 
+  // Output directory
+  std::string outputDir = vm["output-dir"].template as<std::string>();
+
+  // Write fitted tracks as ROOT files
+  std::shared_ptr<FW::Root::RootTrackWriter> tWriterRoot = nullptr;
+  if (vm["output-root"].template as<bool>()) {
+    FW::Root::RootTrackWriter::Config tWriterRootConfig;
+    tWriterRootConfig.collection = fittingConfig.trackCollection;
+    tWriterRootConfig.filePath   = FW::joinPaths(
+                                                 outputDir, fittingConfig.trackCollection + ".root");
+    tWriterRootConfig.treeName   = fittingConfig.trackCollection;
+    auto tWriterRoot
+      = std::make_shared<FW::Root::RootTrackWriter>(tWriterRootConfig);
+
+    sequencer.addWriters({tWriterRoot});
+  }
+
+}
+
+/// @brief Fitting setup
+///
+/// @tparam bfield_t Type of the bfield for the fitting to be set up
+///
+/// @param fieldMap The field map for the fitting setup
+/// @param sequencer The framework sequencer
+/// @param vm The boost variable map to resolve
+/// @param tGeometry The TrackingGeometry for the tracking setup
+/// @param barcodesSvc The barcode service to be used for the fitting 
+template <typename vmap_t>
+void
+setupFitting(vmap_t&                                       vm,
+             FW::Sequencer&                                sequencer,
+             std::shared_ptr<const Acts::TrackingGeometry> tGeometry,
+             std::shared_ptr<FW::BarcodeSvc>               barcodeSvc)
+{
+  // create BField service
+  auto bField = FW::Options::readBField<vmap_t>(vm);
+  // a charged propagator
+  if (bField.first) {
+    // create the shared field
+    using BField = Acts::SharedBField<Acts::InterpolatedBFieldMap>;
+    BField fieldMap(bField.first);
+    // now setup of the fitting algorithm and append it to the sequencer
+    setupFittingAlgorithm(std::move(fieldMap),
+                          sequencer,
+                          vm,
+                          tGeometry,
+                          barcodeSvc);
+  } else {
+    // create the shared field
+    using CField = Acts::ConstantBField;
+    CField fieldMap(*bField.second);
+    // now setup of the fitting algorithm and append it to the sequencer
+    setupFittingAlgorithm(std::move(fieldMap),
+                          sequencer,
+                          vm,
+                          tGeometry,
+                          barcodeSvc);
+  }
+}
