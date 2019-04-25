44ec5d6febc3da707dd65fb5ec345d34c1ebdc32
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2019-04-25 16:26:52 +0200

    empty JsonReader frame

--- Plugins/Json/include/ACTFW/Plugins/Json/JsonMaterialReader.hpp
+++ Plugins/Json/include/ACTFW/Plugins/Json/JsonMaterialReader.hpp
@@ -19,6 +19,10 @@
 #include "Acts/Utilities/Definitions.hpp"
 #include "Acts/Utilities/GeometryID.hpp"
 #include "Acts/Utilities/Logger.hpp"
+#include "Acts/Plugins/Json/lib/json.hpp"
+
+// Convenience shorthand
+using json = nlohmann::json;
 
 namespace Acts {
 
@@ -30,11 +34,11 @@ namespace FW {
 
 namespace Json {
 
-  /// @class JsonProtoSurfaceMaterialReader
+  /// @class JsonMaterialReader
   ///
   /// @brief Read the collection of SurfaceMaterial from a file in order to
   /// load it onto the TrackingGeometry
-  class JsonProtoSurfaceMaterialReader
+  class JsonMaterialReader
       : public FW::IReaderT<Acts::SurfaceMaterialMap>
   {
   public:
@@ -43,32 +47,40 @@ namespace Json {
     class Config
     {
     public:
+      /// The geometry version
+      std::string geoversion = "undefined";    
       /// The volume identification string
-      std::string voltag = "vol";
+      std::string voltag = "volumes";
+      /// The boundary surface string
+      std::string boutag = "boundaries";
       /// The layer identification string
-      std::string laytag = "lay";
+      std::string laytag = "layers";
+      /// The volume material string
+      std::string mattag = "material";
       /// The approach identification string
-      std::string apptag = "app";
+      std::string apptag = "approach";
       /// The sensitive identification string
-      std::string sentag = "sen";
+      std::string sentag = "sensitive";
+      /// The representing idntification string
+      std::string reptag = "representing";
       /// The value tag -> binning values: binZ, binR, binPhi, etc.
-      std::string vtag = "v";
+      std::string bin0tag = "bin0";
       /// The bin position tag
-      std::string ntag = "n";
+      std::string bin1tag = "bin1";
       /// The option tag -> binning options: open, closed
-      std::string otag = "o";
+      std::string otag = "option";
       /// The name of the output file
-      std::string fileName = "proto-maps.json";
+      std::string fileName = "protomaps.json";
       /// The default logger
       std::shared_ptr<const Acts::Logger> logger;
-      // The name of the writer
+      /// The name of the writer
       std::string name = "";
-
+      
       /// Constructor
       ///
       /// @param lname Name of the writer tool
       /// @param lvl The output logging level
-      Config(const std::string&   lname = "ProtoMaterialReader",
+      Config(const std::string&   lname = "JsonMaterialReader",
              Acts::Logging::Level lvl   = Acts::Logging::INFO)
         : logger(Acts::getDefaultLogger(lname, lvl)), name(lname)
       {
@@ -78,10 +90,10 @@ namespace Json {
     /// Constructor
     ///
     /// @param cfg configuration struct for the reader
-    JsonProtoSurfaceMaterialReader(const Config& cfg);
+    JsonMaterialReader(const Config& cfg);
 
     /// Destructor
-    ~JsonProtoSurfaceMaterialReader() override = default;
+    ~JsonMaterialReader() override = default;
 
     /// Framework name() method
     std::string
@@ -99,6 +111,10 @@ namespace Json {
          const FW::AlgorithmContext* ctx  = nullptr) final override;
 
   private:
+    /// Create the Surface Material
+    std::shared_ptr<const Acts::ISurfaceMaterial> 
+    loadSurfaceMaterial(json material);    
+      
     /// The config class
     Config m_cfg;
 
@@ -114,7 +130,7 @@ namespace Json {
   };
 
   inline std::string
-  JsonProtoSurfaceMaterialReader::name() const
+  JsonMaterialReader::name() const
   {
     return m_cfg.name;
   }
@@ -123,7 +139,7 @@ namespace Json {
   class JsonProtoMaterialDecorator : public Acts::IMaterialDecorator
   {
   public:
-    JsonProtoMaterialDecorator(JsonProtoSurfaceMaterialReader::Config rConfig,
+    JsonProtoMaterialDecorator(JsonMaterialReader::Config rConfig,
                                bool clearSurfaceMaterial = true,
                                bool clearVolumeMaterial  = true)
       : m_readerConfig(rConfig)
@@ -131,7 +147,7 @@ namespace Json {
       , m_clearVolumeMaterial(clearVolumeMaterial)
     {
       // Create the reader with the config
-      JsonProtoSurfaceMaterialReader sreader(m_readerConfig);
+      JsonMaterialReader sreader(m_readerConfig);
       // Read the map & return it
       sreader.read(m_surfaceMaterialMap);
     }
@@ -167,7 +183,7 @@ namespace Json {
     }
 
   private:
-    JsonProtoSurfaceMaterialReader::Config m_readerConfig;
+    JsonMaterialReader::Config m_readerConfig;
     Acts::SurfaceMaterialMap               m_surfaceMaterialMap;
 
     bool m_clearSurfaceMaterial{true};
