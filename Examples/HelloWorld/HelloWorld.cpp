abfc32b786248b599e0b7315a2116295501f161b
Moritz Kiehn <msmk@cern.ch>
2019-07-16 19:13:02 +0200

    helloworld: some cleanup

--- Examples/HelloWorld/HelloWorld.cpp
+++ Examples/HelloWorld/HelloWorld.cpp
@@ -1,38 +1,48 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2017 Acts project team
+// Copyright (C) 2017,2019 Acts project team
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+/// @file
+/// @brief An example tools that shows the sequencer functionality
+
 #include <cstdlib>
 #include <memory>
 
+#include "ACTFW/Framework/RandomNumbers.hpp"
 #include "ACTFW/Framework/Sequencer.hpp"
 #include "ACTFW/Options/CommonOptions.hpp"
+
 #include "HelloWorldAlgorithm.hpp"
 
-/// Main read evgen executable
-///
-/// @param argc The argument count
-/// @param argv The argument list
 int
 main(int argc, char* argv[])
 {
-  // setup and parse options
+  // setup options
+  // every component should have an associated option setup function
+  // that should be called here.
   auto opt = FW::Options::makeDefaultOptions();
   FW::Options::addSequencerOptions(opt);
+  FW::Options::addRandomNumbersOptions(opt);
+  // parse options from command line flags
   auto vm = FW::Options::parse(opt, argc, argv);
-  if (vm.empty()) {
-    return EXIT_FAILURE;
-  }
+  // an empty varaibles map indicates an error
+  if (vm.empty()) { return EXIT_FAILURE; }
 
-  FW::Sequencer sequencer(FW::Options::readSequencerConfig(vm));
-
+  // extract some common options
   auto logLevel = FW::Options::readLogLevel(vm);
 
-  // add w/ HelloWorld algorithm
+  // setup basic tools shared among algorithms
+  auto rnd = std::make_shared<FW::RandomNumbers>(
+      FW::Options::readRandomNumbersConfig(vm));
+
+  // setup the sequencer first w/ config derived from options
+  FW::Sequencer sequencer(FW::Options::readSequencerConfig(vm));
+
+  // add HelloWorld algorithm that does nothing
   sequencer.addAlgorithm(std::make_shared<FW::HelloWorldAlgorithm>(logLevel));
 
   return sequencer.run();
--- Examples/HelloWorld/HelloWorldAlgorithm.hpp
+++ Examples/HelloWorld/HelloWorldAlgorithm.hpp
@@ -1,34 +1,28 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2017 Acts project team
+// Copyright (C) 2017,2019 Acts project team
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#ifndef ACTFW_EXAMPLES_HELLOWORLD_H
-#define ACTFW_EXAMPLES_HELLOWORLD_H 1
+#pragma once
 
 #include <memory>
 
 #include "ACTFW/Framework/BareAlgorithm.hpp"
-#include "ACTFW/Framework/ProcessCode.hpp"
 
 namespace FW {
 
-/// @class Algorithm
+/// A simple algorithm that just prints hello world.
 class HelloWorldAlgorithm : public FW::BareAlgorithm
 {
 public:
-  /// Constructor
-  HelloWorldAlgorithm(Acts::Logging::Level level = Acts::Logging::INFO);
+  HelloWorldAlgorithm(Acts::Logging::Level level);
 
-  /// Framework execute method
-  /// @param [in] context is the Algorithm context for event consistency
+  // Per-event method.
   FW::ProcessCode
   execute(const AlgorithmContext& context) const final override;
 };
 
 }  // namespace FW
-
-#endif  // ACTFW_EXAMPLES_HELLOWORLD_H
