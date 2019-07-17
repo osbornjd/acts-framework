89da2088ef72b46e8d4def060c1c9f09ca7c9008
Moritz Kiehn <msmk@cern.ch>
2019-07-17 08:52:05 +0200

    core: add BareService implementation helper

---
+++ Core/src/Framework/BareService.cpp
@@ -0,0 +1,36 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2019 Acts project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ACTFW/Framework/BareService.hpp"
+
+namespace FW {
+
+BareService::BareService(std::string name, Acts::Logging::Level level)
+  : m_name(std::move(name)), m_logger(Acts::getDefaultLogger(m_name, level))
+{
+}
+
+std::string
+BareService::name() const
+{
+  return m_name;
+}
+
+void
+BareService::startRun()
+{
+  // nothing to do in the default implementation
+}
+
+void
+BareService::prepare(AlgorithmContext& ctx)
+{
+  // nothing to do in the default implementation
+}
+
+}  // namespace FW
