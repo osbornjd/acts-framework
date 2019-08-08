638dd7d667cd1557a4ecaaf7e8996aef5e612832
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2019-08-08 14:33:29 +0200

    Pixel Barrel detector

---
+++ Detectors/DD4hepDetector/src/OpenDataDetector/ODDCylinder_geo.cpp
@@ -0,0 +1,55 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2019 Acts project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "Acts/Plugins/DD4hep/ActsExtension.hpp"
+#include "DD4hep/DetFactoryHelper.h"
+
+using namespace std;
+using namespace dd4hep;
+
+static Ref_t
+create_element(Detector& lcdd, xml_h xml, SensitiveDetector sens)
+{
+
+  xml_det_t x_det   = xml;
+  string    detName = x_det.nameStr();
+
+  // Make DetElement
+  DetElement beamPipeElement(detName, x_det.id());
+
+  // add Extension to Detlement for the RecoGeometry
+  Acts::ActsExtension::Config volConfig;
+  bool                        isBeamPipe = x_det.hasChild(_U(beampipe));
+  if (isBeamPipe) {
+    volConfig.isBeampipe = true;
+  } else {
+    volConfig.isBarrel = true;
+  }
+  Acts::ActsExtension* detvolume = new Acts::ActsExtension(volConfig);
+  beamPipeElement.addExtension<Acts::IActsExtension>(detvolume);
+
+  // Make Volume
+  xml_comp_t x_det_tubs = x_det.child(_U(tubs));
+  string     shapeName  = x_det_tubs.nameStr();
+  Tube       beamPipeShape(
+      shapeName, x_det_tubs.rmin(), x_det_tubs.rmax(), x_det_tubs.dz());
+  Volume tube_vol(
+      detName, beamPipeShape, lcdd.material(x_det_tubs.materialStr()));
+  tube_vol.setVisAttributes(lcdd, x_det.visStr());
+
+  // Place it in the mother
+  Volume       mother_vol = lcdd.pickMotherVolume(beamPipeElement);
+  PlacedVolume placedTube = mother_vol.placeVolume(tube_vol);
+  placedTube.addPhysVolID("BeamPipeVolume", beamPipeElement.id());
+  beamPipeElement.setPlacement(placedTube);
+
+  // And return it
+  return beamPipeElement;
+}
+
+DECLARE_DETELEMENT(ODDCylinder, create_element)
---
+++ Detectors/DD4hepDetector/src/OpenDataDetector/ODDPixelBarrel_geo.cpp
@@ -0,0 +1,257 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2019 Acts project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "Acts/Plugins/DD4hep/ActsExtension.hpp"
+#include "DD4hep/DetFactoryHelper.h"
+
+using namespace std;
+using namespace dd4hep;
+
+static Ref_t
+create_element(Detector& lcdd, xml_h xml, SensitiveDetector sens)
+{
+
+  xml_det_t x_det   = xml;
+  string    detName = x_det.nameStr();
+
+  // Make DetElement
+  DetElement barrelDetector(detName, x_det.id());
+
+  // Add Extension to DetElement for the RecoGeometry
+  Acts::ActsExtension::Config volConfig;
+  volConfig.isBarrel             = true;
+  Acts::ActsExtension* detvolume = new Acts::ActsExtension(volConfig);
+  barrelDetector.addExtension<Acts::IActsExtension>(detvolume);
+
+  // Make Volume
+  dd4hep::xml::Dimension x_det_dim(x_det.dimensions());
+  string                 barrelShapeName = x_det_dim.nameStr();
+
+  // The Shape and Volume
+  Tube   barrelShape(x_det_dim.rmin(), x_det_dim.rmax(), x_det_dim.dz());
+  Volume barrelVolume(detName, barrelShape, lcdd.air());
+  barrelVolume.setVisAttributes(lcdd, x_det.visStr());
+
+  // Create the stave volume and DetElement tree
+  xml_comp_t x_stave = x_det.child(_U(stave));
+  Assembly   staveAssembly("stave");
+  // Visualization
+  staveAssembly.setVisAttributes(lcdd, x_stave.visStr());
+
+  // The DetElement tree, layer inside barrel
+  // DetElement staveElement(barrelDetector, layerName, layerNum);
+
+  // Create the module components
+  xml_comp_t x_module = x_det.child(_U(module));
+
+  // The Module envelope volume
+  Assembly moduleAssembly("module");
+  // Visualization
+  moduleAssembly.setVisAttributes(lcdd, x_module.visStr());
+
+  // Place the components inside the module
+  unsigned int compNum   = 0;
+  unsigned int sensorNum = 0;
+  double       ylength   = 0.;
+  for (xml_coll_t comp(x_module, _U(module_component)); comp;
+       ++comp, ++compNum) {
+    xml_comp_t x_comp = comp;
+    // Component volume
+    string componentName = _toString((int)compNum, "component%d");
+    Volume componentVolume(
+        componentName,
+        Box(0.5 * x_comp.dx(), 0.5 * x_comp.dy(), 0.5 * x_comp.dz()),
+        lcdd.material(x_comp.materialStr()));
+    componentVolume.setVisAttributes(lcdd, x_comp.visStr());
+
+    // calculate the module dimension
+    double cylength
+        = 2. * abs(std::copysign(0.5 * x_comp.dy(), x_comp.y_offset())
+                   + x_comp.y_offset());
+    ylength = cylength > ylength ? cylength : ylength;
+
+    // Visualization
+    componentVolume.setVisAttributes(lcdd, x_comp.visStr());
+    // Place Module Box Volumes in layer
+    PlacedVolume placedComponent = moduleAssembly.placeVolume(
+        componentVolume,
+        Position(x_comp.x_offset(), x_comp.y_offset(), x_comp.z_offset()));
+
+    if (x_comp.isSensitive()) {
+      componentVolume.setSensitiveDetector(sens);
+      placedComponent.addPhysVolID("sensor", ++sensorNum);
+    }
+  }
+
+  // Place the modules into the stave
+  double       gap          = x_stave.gap();
+  unsigned int nModules     = x_stave.nmodules();
+  double       ystep        = ylength + gap;
+  double       ymin         = (nModules * 0.5 - 0.5) * ylength;
+  double       staveHlength = ymin + 0.5 * ylength;
+
+  // Loop over the modules and place them
+  for (unsigned int moduleNum = 0; moduleNum < nModules; ++moduleNum) {
+    // Place them along local y
+    PlacedVolume placedModule = staveAssembly.placeVolume(
+        moduleAssembly, Position(0., -ymin + moduleNum * ystep, 0.));
+    placedModule.addPhysVolID("module", moduleNum);
+  }
+
+  // Place carbon foam structure
+  if (x_stave.hasChild(_U(subtraction)) and x_stave.hasChild(_U(tube))) {
+    xml_comp_t x_sub  = x_stave.child(_U(subtraction));
+    xml_comp_t x_trd  = x_sub.child(_U(trd));
+    xml_comp_t x_tubs = x_sub.child(_U(tubs));
+    xml_comp_t x_pipe = x_stave.child(_U(tube));
+
+    // Create the two shapes first
+    Trapezoid foamShape(x_trd.x1(),
+                        x_trd.x2(),
+                        staveHlength + x_trd.dz(),
+                        staveHlength + x_trd.dz(),
+                        x_trd.thickness());
+
+    Tube foamCutout(x_tubs.rmin(), x_tubs.rmax(), staveHlength + x_tubs.dz());
+
+    // Create the substraction
+    Volume foamVolume("CarbonFoam",
+                      SubtractionSolid(foamShape,
+                                       foamCutout,
+                                       Transform3D(RotationX(0.5 * M_PI))),
+                      lcdd.material(x_sub.materialStr()));
+    foamVolume.setVisAttributes(lcdd, x_sub.visStr());
+    // Place the volume in the stave
+    PlacedVolume placedFoam = staveAssembly.placeVolume(
+        foamVolume,
+        Position(x_sub.x_offset(), x_sub.y_offset(), x_sub.z_offset()));
+    // placedFoam.addPhysVolID("component", ++compNum);
+
+    Tube coolingPipe(x_pipe.rmin(), x_pipe.rmax(), staveHlength + x_pipe.dz());
+    // Create the substraction
+    Volume pipeVolume(
+        "CoolingPipe", coolingPipe, lcdd.material(x_pipe.materialStr()));
+    pipeVolume.setVisAttributes(lcdd, x_pipe.visStr());
+
+    // Place the pipe in the stave
+    PlacedVolume placedPipe = staveAssembly.placeVolume(
+        pipeVolume,
+        Transform3D(
+            RotationX(0.5 * M_PI),
+            Position(x_pipe.x_offset(), x_pipe.y_offset(), x_pipe.z_offset())));
+
+    // placedPipe.addPhysVolID("component", ++compNum);
+
+    xml_comp_t x_cable = x_stave.child(_U(eltube));
+    // Place the support cables for the modules
+    for (unsigned int modCable = 0; modCable < 0.5 * nModules; ++modCable) {
+
+      double cableLength = staveHlength - modCable * ylength;
+
+      for (int side = -1; side < 2; side += 2) {
+        Tube cable(x_cable.rmin(), x_cable.rmax(), 0.5 * cableLength);
+        // Create the substraction
+        Volume cableVolume("Cable", cable, lcdd.material(x_pipe.materialStr()));
+        cableVolume.setVisAttributes(lcdd, x_pipe.visStr());
+
+        // Place the pipe in the stave
+        PlacedVolume placedCable = staveAssembly.placeVolume(
+            cableVolume,
+            Transform3D(
+                RotationX(0.5 * M_PI),
+                Position(
+                    x_cable.x_offset() + 2.05 * modCable * x_cable.rmax(),
+                    side * (staveHlength - 0.5 * cableLength + x_cable.dz()),
+                    x_cable.z_offset())));
+      }
+    }
+  }
+
+  // Loop over the layers to build staves
+  size_t layerNum = 0;
+  for (xml_coll_t lay(xml, _U(layer)); lay; ++lay, ++layerNum) {
+    xml_comp_t x_layer = lay;
+
+    string layerName = _toString((int)layerNum, "layer%d");
+    // The Module envelope volume
+    Volume layerVolume(layerName,
+                       Tube(x_layer.rmin(), x_layer.rmax(), x_layer.dz()),
+                       lcdd.air());
+    // Visualization
+    layerVolume.setVisAttributes(lcdd, x_layer.visStr());
+
+    // The DetElement tree, keep it flat
+    DetElement layerElement(barrelDetector, layerName, layerNum);
+
+    // Place the staves in the layer
+    unsigned int nStaves = x_layer.nphi();
+    double       phiStep = 2. * M_PI / nStaves;
+    double       phiTilt = x_layer.phi_tilt();
+    double       phi0    = x_layer.phi0();
+    double       r       = x_layer.r();
+
+    // Loop over the staves and place them
+    for (unsigned int staveNum = 0; staveNum < nStaves; ++staveNum) {
+      string staveName = _toString((int)staveNum, "stave%d");
+      // position of the stave
+      double phi = phi0 + staveNum * phiStep;
+      double x   = r * cos(phi);
+      double y   = r * sin(phi);
+      // Now place the stave
+      PlacedVolume placedStave = layerVolume.placeVolume(
+          staveAssembly,
+          Transform3D(RotationY(0.5 * M_PI) * RotationZ(0.5 * M_PI)
+                          * RotationY(phi + phiTilt),
+                      Position(x, y, 0.)));
+      placedStave.addPhysVolID("stave", staveNum);
+    }
+
+    // Place the support cylinder
+    if (x_layer.hasChild(_U(support))) {
+
+      xml_comp_t x_support = x_layer.child(_U(support));
+      // Create the volume of the support structure
+      Volume supportVolume(
+          "SupportCylinder",
+          Tube(x_support.rmin(), x_support.rmax(), x_support.dz()),
+          lcdd.material(x_support.materialStr()));
+      supportVolume.setVisAttributes(lcdd, x_support.visStr());
+      // Place the support structure
+      PlacedVolume placedSupport = layerVolume.placeVolume(supportVolume);
+      // placedSupport.addPhysVolID("component", ++compNum);
+    }
+
+    // Place the layer with appropriate Acts::Extension
+    // Configure the ACTS extension
+    Acts::ActsExtension::Config layerConfig;
+    layerConfig.isLayer                 = true;
+    Acts::ActsExtension* layerExtension = new Acts::ActsExtension(layerConfig);
+    layerElement.addExtension<Acts::IActsExtension>(layerExtension);
+
+    PlacedVolume placedLayer = barrelVolume.placeVolume(layerVolume);
+    placedLayer.addPhysVolID("layer", layerNum);
+
+    // Assign layer DetElement to layer volume
+    layerElement.setPlacement(placedLayer);
+  }
+
+  // Place Volume
+  Volume       motherVolume = lcdd.pickMotherVolume(barrelDetector);
+  Position     translation(0., 0., x_det_dim.z());
+  PlacedVolume placedBarrel
+      = motherVolume.placeVolume(barrelVolume, translation);
+  // "system" is hard coded in the DD4Hep::VolumeManager
+  placedBarrel.addPhysVolID("barrelendcap", 0);
+  placedBarrel.addPhysVolID("system", barrelDetector.id());
+  barrelDetector.setPlacement(placedBarrel);
+
+  // And return the detector element
+  return barrelDetector;
+}
+
+DECLARE_DETELEMENT(ODDPixelBarrel, create_element)
