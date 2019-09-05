fe2514435310818464ba89f40598ef56c1325cf0
Moritz Kiehn <msmk@cern.ch>
2019-09-05 21:29:16 +0200

    examples: update csv reader to new detector build

--- Examples/ReadCsv/GenericReadCsvExample.cpp
+++ Examples/ReadCsv/GenericReadCsvExample.cpp
@@ -1,21 +1,20 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2017 Acts project team
+// Copyright (C) 2019 Acts project team
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#pragma once
+#include <memory>
 
 #include <boost/program_options.hpp>
-#include <memory>
-#include "ACTFW/Barcode/BarcodeSvc.hpp"
-#include "ACTFW/Common/CommonOptions.hpp"
-#include "ACTFW/Common/GeometryOptions.hpp"
-#include "ACTFW/Common/OutputOptions.hpp"
+
+#include "ACTFW/EventData/Barcode.hpp"
 #include "ACTFW/Framework/Sequencer.hpp"
 #include "ACTFW/Framework/WhiteBoard.hpp"
+#include "ACTFW/GenericDetector/GenericDetector.hpp"
+#include "ACTFW/Geometry/CommonGeometry.hpp"
 #include "ACTFW/Options/CommonOptions.hpp"
 #include "ACTFW/Plugins/Csv/CsvParticleReader.hpp"
 #include "ACTFW/Plugins/Csv/CsvPlanarClusterReader.hpp"
@@ -23,24 +22,8 @@
 #include "ACTFW/Plugins/Csv/CsvReaderOptions.hpp"
 #include "ACTFW/Utilities/Options.hpp"
 
-namespace po = boost::program_options;
-
-/// @brief The ReadCsv example
-///
-/// @tparam options_setup_t are the callable example options
-/// @tparam geometry_setup_t Type of the geometry getter struct
-///
-/// @param argc the number of argumetns of the call
-/// @param argv the argument list
-/// @param optionsSetup is a callable options struct
-/// @param geometrySetup is a callable geometry getter
-template <typename options_setup_t, typename geometry_setup_t>
 int
-CsvExample(int               argc,
-           char*             argv[],
-           options_setup_t&  optionsSetup,
-           geometry_setup_t& geometrySetup)
-
+main(int argc, char* argv[])
 {
   // setup and parse options
   auto desc = FW::Options::makeDefaultOptions();
@@ -49,25 +32,23 @@ CsvExample(int               argc,
   FW::Options::addCsvReaderOptions(desc);
   FW::Options::addOutputOptions(desc);
 
-  optionsSetup(desc);
+  GenericDetector detector;
+  detector.addOptions(desc);
+
   auto vm = FW::Options::parse(desc, argc, argv);
-  if (vm.empty()) {
-    return EXIT_FAILURE;
-  }
+  if (vm.empty()) { return EXIT_FAILURE; }
 
   FW::Sequencer sequencer(FW::Options::readSequencerConfig(vm));
 
   // Now read the standard options
-  auto logLevel          = FW::Options::readLogLevel(vm);
-  auto nEvents           = FW::Options::readSequencerConfig(vm).events;
-  auto geometry          = geometrySetup(vm);
-  auto tGeometry         = geometry.first;
-  auto contextDecorators = geometry.second;
+  auto logLevel = FW::Options::readLogLevel(vm);
+  auto nEvents  = FW::Options::readSequencerConfig(vm).events;
 
-  // Add it to the sequencer
-  for (auto cdr : contextDecorators) {
-    sequencer.addContextDecorator(cdr);
-  }
+  // setup detector geometry
+  auto geometry         = FW::Geometry::build(vm, detector);
+  auto trackingGeometry = geometry.first;
+  // Add context decorators
+  for (auto cdr : geometry.second) { sequencer.addContextDecorator(cdr); }
 
   // Input directory
   std::string inputDir = vm["input-dir"].as<std::string>();
@@ -97,7 +78,7 @@ CsvExample(int               argc,
   // Read clusters as CSV files
   if (vm["read-plCluster-csv"].as<bool>()) {
     FW::Csv::CsvPlanarClusterReader::Config plCsvReaderConfig;
-    plCsvReaderConfig.tGeometry               = tGeometry;
+    plCsvReaderConfig.tGeometry               = trackingGeometry;
     plCsvReaderConfig.inputDir                = inputDir;
     plCsvReaderConfig.inputHitsFileName       = inputHitsFileName + ".csv";
     plCsvReaderConfig.inputDetailsFileName    = inputDetailsFileName + ".csv";
