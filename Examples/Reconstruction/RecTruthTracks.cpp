6abe3691100279903f12ffa9eef851431918b8bd
Moritz Kiehn <msmk@cern.ch>
2019-09-23 17:55:41 +0200

    add standalone truth reconstruction tool

---
+++ Examples/Reconstruction/RecTruthTracks.cpp
@@ -0,0 +1,148 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2019 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <memory>
+
+#include <Acts/Utilities/Units.hpp>
+
+#include "ACTFW/Digitization/HitSmearing.hpp"
+#include "ACTFW/EventData/Barcode.hpp"
+#include "ACTFW/Fitting/FittingAlgorithm.hpp"
+#include "ACTFW/Framework/Sequencer.hpp"
+#include "ACTFW/Framework/WhiteBoard.hpp"
+#include "ACTFW/GenericDetector/GenericDetector.hpp"
+#include "ACTFW/Geometry/CommonGeometry.hpp"
+#include "ACTFW/Io/Performance/TrackFinderPerformanceWriter.hpp"
+#include "ACTFW/Options/CommonOptions.hpp"
+#include "ACTFW/Plugins/BField/BFieldOptions.hpp"
+#include "ACTFW/Plugins/Csv/CsvOptionsReader.hpp"
+#include "ACTFW/Plugins/Csv/CsvParticleReader.hpp"
+#include "ACTFW/Plugins/Csv/CsvPlanarClusterReader.hpp"
+#include "ACTFW/TruthTracking/ParticleSmearing.hpp"
+#include "ACTFW/TruthTracking/TruthTrackFinder.hpp"
+#include "ACTFW/Utilities/Options.hpp"
+#include "ACTFW/Utilities/Paths.hpp"
+
+using namespace Acts::UnitLiterals;
+using namespace FW;
+
+int
+main(int argc, char* argv[])
+{
+  GenericDetector detector;
+
+  // setup and parse options
+  auto desc = FW::Options::makeDefaultOptions();
+  Options::addSequencerOptions(desc);
+  Options::addRandomNumbersOptions(desc);
+  Options::addGeometryOptions(desc);
+  Options::addMaterialOptions(desc);
+  Options::addInputOptions(desc);
+  Options::addOutputOptions(desc);
+  detector.addOptions(desc);
+  Options::addBFieldOptions(desc);
+
+  auto vm = Options::parse(desc, argc, argv);
+  if (vm.empty()) { return EXIT_FAILURE; }
+
+  Sequencer sequencer(Options::readSequencerConfig(vm));
+
+  // Read some standard options
+  auto logLevel  = Options::readLogLevel(vm);
+  auto inputDir  = vm["input-dir"].as<std::string>();
+  auto outputDir = vm["output-dir"].as<std::string>();
+  auto rnd       = std::make_shared<FW::RandomNumbers>(
+      Options::readRandomNumbersConfig(vm));
+  // ensure the output directory exists
+  ensureWritableDirectory(outputDir);
+
+  // Setup detector geometry
+  auto geometry         = Geometry::build(vm, detector);
+  auto trackingGeometry = geometry.first;
+  // Add context decorators
+  for (auto cdr : geometry.second) { sequencer.addContextDecorator(cdr); }
+  // Setup the magnetic field
+  auto magneticField = Options::readBField(vm);
+
+  // Read particles and clusters from CSV files
+  auto particleReaderCfg            = Options::readCsvParticleReaderConfig(vm);
+  particleReaderCfg.outputParticles = "truth_particles";
+  sequencer.addReader(
+      std::make_shared<Csv::CsvParticleReader>(particleReaderCfg, logLevel));
+  // Read clusters from CSV files
+  auto clusterReaderCfg = Options::readCsvPlanarClusterReaderConfig(vm);
+  clusterReaderCfg.trackingGeometry = trackingGeometry;
+  // TODO read truth hits
+  clusterReaderCfg.outputClusters        = "clusters";
+  clusterReaderCfg.outputHitIds          = "hit_ids";
+  clusterReaderCfg.outputHitParticlesMap = "truth_hit_particles_map";
+  clusterReaderCfg.outputSimulatedHits   = "truth_hits";
+  sequencer.addReader(std::make_shared<Csv::CsvPlanarClusterReader>(
+      clusterReaderCfg, logLevel));
+
+  // Create smeared measurements
+  HitSmearing::Config hitSmearingCfg;
+  hitSmearingCfg.inputSimulatedHits = clusterReaderCfg.outputSimulatedHits;
+  hitSmearingCfg.outputSourceLinks  = "sourcelinks";
+  hitSmearingCfg.sigmaLoc0          = 25_um;
+  hitSmearingCfg.sigmaLoc1          = 100_um;
+  hitSmearingCfg.randomNumbers      = rnd;
+  sequencer.addAlgorithm(
+      std::make_shared<HitSmearing>(hitSmearingCfg, logLevel));
+
+  // TODO pre-select particles
+
+  // The fitter needs the measurements (proto tracks) and initial track states
+  // (proto states). The elements in both collections must match and must be
+  // created from the same input particles. Create truth tracks
+  TruthTrackFinder::Config trackFinderCfg;
+  trackFinderCfg.inputParticles       = particleReaderCfg.outputParticles;
+  trackFinderCfg.inputHitParticlesMap = clusterReaderCfg.outputHitParticlesMap;
+  trackFinderCfg.outputProtoTracks    = "prototracks";
+  sequencer.addAlgorithm(
+      std::make_shared<TruthTrackFinder>(trackFinderCfg, logLevel));
+  // Create smeared particles states
+  ParticleSmearing::Config particleSmearingCfg;
+  particleSmearingCfg.inputParticles    = particleReaderCfg.outputParticles;
+  particleSmearingCfg.outputTrackStates = "protostates";
+  particleSmearingCfg.randomNumbers     = rnd;
+  // Gaussian sigmas to smear particle parameters
+  particleSmearingCfg.sigmaD0    = 20_um;
+  particleSmearingCfg.sigmaD0PtA = 30_um;
+  particleSmearingCfg.sigmaD0PtB = 0.3 / 1_GeV;
+  particleSmearingCfg.sigmaZ0    = 20_um;
+  particleSmearingCfg.sigmaZ0PtA = 30_um;
+  particleSmearingCfg.sigmaZ0PtB = 0.3 / 1_GeV;
+  particleSmearingCfg.sigmaPhi   = 1_degree;
+  particleSmearingCfg.sigmaTheta = 1_degree;
+  particleSmearingCfg.sigmaPRel  = 0.01;
+  particleSmearingCfg.sigmaT0    = 1_ns;
+  sequencer.addAlgorithm(
+      std::make_shared<ParticleSmearing>(particleSmearingCfg, logLevel));
+
+  // setup the fitter
+  FittingAlgorithm::Config fitCfg;
+  fitCfg.inputSourceLinks   = hitSmearingCfg.outputSourceLinks;
+  fitCfg.inputProtoTracks   = trackFinderCfg.outputProtoTracks;
+  fitCfg.inputProtoStates   = particleSmearingCfg.outputTrackStates;
+  fitCfg.outputTrajectories = "trajectories";
+  fitCfg.fit                = FittingAlgorithm::makeFitterFunction(
+      trackingGeometry, magneticField, logLevel);
+  sequencer.addAlgorithm(std::make_shared<FittingAlgorithm>(fitCfg, logLevel));
+
+  // write reconstruction performance data
+  TrackFinderPerformanceWriter::Config perfCfg;
+  perfCfg.inputParticles       = particleReaderCfg.outputParticles;
+  perfCfg.inputHitParticlesMap = clusterReaderCfg.outputHitParticlesMap;
+  perfCfg.inputProtoTracks     = trackFinderCfg.outputProtoTracks;
+  perfCfg.outputDir            = outputDir;
+  sequencer.addWriter(
+      std::make_shared<TrackFinderPerformanceWriter>(perfCfg, logLevel));
+
+  return sequencer.run();
+}
