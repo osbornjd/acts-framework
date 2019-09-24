b374e19c588cc0b2cfc5842e153cb7e9525a3169
Moritz Kiehn <msmk@cern.ch>
2019-09-24 13:56:27 +0200

    Io: add track finder performance writer

---
+++ Core/include/ACTFW/Validation/ProtoTrackClassification.hpp
@@ -0,0 +1,34 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2019 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "ACTFW/EventData/Barcode.hpp"
+#include "ACTFW/EventData/DataContainers.hpp"
+#include "ACTFW/EventData/ProtoTrack.hpp"
+
+namespace FW {
+
+/// Associate a particle to its hit count within a proto track.
+struct ParticleHitCount
+{
+  barcode_type particleId;
+  size_t       hitCount;
+};
+
+/// Identify all particles that contribute to the proto track and count hits.
+void
+identifyContributingParticles(
+    const IndexMultimap<barcode_type>& hitParticlesMap,
+    const ProtoTrack&                  protoTrack,
+    std::vector<ParticleHitCount>&     particleHitCount);
+
+}  // namespace FW
