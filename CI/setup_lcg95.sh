612f7d75665a8411eed4fc232226f252cd82aa68
Moritz Kiehn <msmk@cern.ch>
2019-09-24 15:55:57 +0200

    CI: use LCG95

--- CI/setup_lcg95.sh
+++ CI/setup_lcg95.sh
@@ -6,8 +6,14 @@ source $DIR/env_info.sh
 
 release=LCG_95
 platform=x86_64-${ACTS_OS}-${ACTS_COMPILER}
-lcg=/cvmfs/sft.cern.ch/lcg/views/${release}/${platform}
+basedir=/cvmfs/sft.cern.ch/lcg
+lcg=${basedir}/views/${release}/${platform}
 
 source ${lcg}/setup.sh
 # extra variables required to build acts
 export DD4hep_DIR=${lcg}
+export PYTHIA8_INCLUDE_DIR="${lcg}/include"
+export PYTHIA8_LIBRARY_DIR="${lcg}/lib"
+export HEPMC3_DIR="${basedir}/releases/${release}/hepmc3/3.0.0/${platform}"
+export HEPPID_INCLUDE_DIR="${lcg}/include"
+export HEPPID_LIBRARY_DIR="${lcg}/lib"
