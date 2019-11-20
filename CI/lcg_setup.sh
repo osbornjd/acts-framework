21546a33e1c5ba6625e7d73ee840cf0f53624682
Paul Gessinger <paul.gessinger@cern.ch>
2019-11-20 13:11:01 +0100

    Make lcg setup script independent of pwd

--- CI/lcg_setup.sh
+++ CI/lcg_setup.sh
@@ -1,7 +1,8 @@
 # setup a LCG release via cvmfs
 #
 # must be called from the repository root
+dir=$(dirname $(realpath $BASH_SOURCE))
  
-source CI/lcg_variables.sh
+source $dir/lcg_variables.sh
 source ${lcg_view}/setup.sh
 echo "using ${lcg_release} on ${lcg_platform}"
