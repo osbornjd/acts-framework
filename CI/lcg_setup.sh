8f1d1b020e776c16eb8de0a18124c7fb584b9c24
Paul Gessinger <paul.gessinger@cern.ch>
2019-11-25 14:01:04 +0100

    add case for zsh too

--- CI/lcg_setup.sh
+++ CI/lcg_setup.sh
@@ -1,8 +1,20 @@
 # setup a LCG release via cvmfs
 #
 # must be called from the repository root
-dir=$(dirname $(realpath $BASH_SOURCE))
- 
+if [ -n "$BASH_SOURCE" ]; then
+  this_script=$BASH_SOURCE
+elif [ -n "$ZSH_VERSION" ]; then
+  setopt function_argzero
+  this_script=$0
+else
+  echo 1>&2 "Unsupported shell. Please use bash, or zsh."
+  exit 2
+fi
+
+dir=$(dirname $(realpath $this_script))
+
 source $dir/lcg_variables.sh
+
 source ${lcg_view}/setup.sh
 echo "using ${lcg_release} on ${lcg_platform}"
+
