51159cfcb796b3de9a7a658f41aac85339e6fc41
Moritz Kiehn <msmk@cern.ch>
2019-11-29 21:59:23 +0100

    move fitter performance writer to Io

--- Io/Performance/ACTFW/Io/Performance/TrackFitterPerformanceWriter.cpp
+++ Io/Performance/ACTFW/Io/Performance/TrackFitterPerformanceWriter.cpp
@@ -6,7 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "ACTFW/Plugins/Root/RootPerformanceWriter.hpp"
+#include "ACTFW/Io/Performance/TrackFitterPerformanceWriter.hpp"
 #include <TFile.h>
 #include <TTree.h>
 #include <ios>
