f7f5ae1e6b4607ff33a3c11a7feeaca93ffc36ef
Moritz Kiehn <msmk@cern.ch>
2020-01-21 15:58:57 +0100

    EventData: rework barcode encoding

--- Core/include/ACTFW/EventData/Barcode.hpp
+++ Core/include/ACTFW/EventData/Barcode.hpp
@@ -13,7 +13,39 @@
 
 namespace FW {
 
-/// Particle identifier that encodes some event information.
+/// Particle identifier that encodes additional event information.
+///
+/// The barcode has to fulfill two separate requirements: be able to act as
+/// unique identifier for particles within an event and to encode details
+/// on the event structure for fast lookup. Since we only care about tracking
+/// here, we only need to support two scenarios:
+///
+/// *   Identify which primary/secondary vertex particles belong to. No
+///     information on intermediate/unstable/invisible particles needs to be
+///     retained.
+/// *   If visible particles convert, decay, or interact with the detector, we
+///     need to be able to identify the initial (primary) particle. Typical
+///     examples are pion nuclear interactions or electron/gamma conversions
+///     where the leading secondary particle has similar kinematic properties
+///     to the primary particle.
+///
+/// The vertex information is encoded as two 12bit numbers the define the
+/// primary and secondary vertex. The primary vertex must be non-zero.
+/// Particles with secondary vertex 0 originate directly from the primary
+/// vertex.
+///
+/// Within one vertex (primary+secondary) each particle is identified by a
+/// 16bit number. A second 16bit number stores the parent particle identifier
+/// (as previously defined). A value of zero means that no visible parent
+/// exists. Consequently, zero can not be a valid particle identifier for the
+/// previous field and must not be used.
+///
+/// A 8bit process identifier provides additional information on the process
+/// that created the particle.
+///
+/// A barcode with all elements set to zero (the default value) is an invalid
+/// value that can be used e.g. to mark missing particles.
+///
 class Barcode
 {
 public:
@@ -40,29 +72,29 @@ public:
     return m_value;
   }
 
-  /// Return the vertex identifier.
+  /// Return the primary vertex identifier.
   constexpr Value
-  vertex() const
+  vertexPrimary() const
   {
-    return getBits(Masks::Vertex);
+    return getBits(Masks::VertexPrimary);
   }
-  /// Return the primary particle identifier.
+  /// Return the secondary vertex identifier.
   constexpr Value
-  primary() const
+  vertexSecondary() const
   {
-    return getBits(Masks::Primary);
+    return getBits(Masks::VertexSecondary);
   }
-  /// Return the generation identifier.
+  /// Return the particle identifier.
   constexpr Value
-  generation() const
+  particle() const
   {
-    return getBits(Masks::Generation);
+    return getBits(Masks::Particle);
   }
-  /// Return the secondary particle identifier.
+  /// Return the parent particle identifier.
   constexpr Value
-  secondary() const
+  parentParticle() const
   {
-    return getBits(Masks::Secondary);
+    return getBits(Masks::ParentParticle);
   }
   /// Return the process identifier.
   constexpr Value
@@ -71,29 +103,29 @@ public:
     return getBits(Masks::Process);
   }
 
-  /// Set the vertex identifier.
+  /// Set the primary vertex identifier.
   constexpr Barcode&
-  setVertex(Value id)
+  setVertexPrimary(Value id)
   {
-    return setBits(Masks::Vertex, id);
+    return setBits(Masks::VertexPrimary, id);
   }
-  /// Set the primary particle identifier.
+  /// Set the secondary vertex identifier.
   constexpr Barcode&
-  setPrimary(Value id)
+  setVertexSecondary(Value id)
   {
-    return setBits(Masks::Primary, id);
+    return setBits(Masks::VertexSecondary, id);
   }
-  /// Set the generation identifier.
+  /// Set the particle identifier.
   constexpr Barcode&
-  setGeneration(Value id)
+  setParticle(Value id)
   {
-    return setBits(Masks::Generation, id);
+    return setBits(Masks::Particle, id);
   }
-  /// Set the secondary particle identifier.
+  /// Set the parent particle identifier.
   constexpr Barcode&
-  setSecondary(Value id)
+  setParentParticle(Value id)
   {
-    return setBits(Masks::Secondary, id);
+    return setBits(Masks::ParentParticle, id);
   }
   /// Set the process identifier.
   constexpr Barcode&
@@ -104,11 +136,11 @@ public:
 
 private:
   enum Masks : Value {
-    Vertex     = UINT64_C(0xfff0000000000000),
-    Primary    = UINT64_C(0x000ffff000000000),
-    Generation = UINT64_C(0x0000000fff000000),
-    Secondary  = UINT64_C(0x0000000000fff000),
-    Process    = UINT64_C(0x0000000000000fff),
+    VertexPrimary   = UINT64_C(0xfff0000000000000),
+    VertexSecondary = UINT64_C(0x000fff0000000000),
+    Particle        = UINT64_C(0x000000ffff000000),
+    ParentParticle  = UINT64_C(0x0000000000ffff00),
+    Process         = UINT64_C(0x00000000000000ff),
   };
 
   Value m_value = 0;
@@ -151,8 +183,8 @@ private:
   friend std::ostream&
   operator<<(std::ostream& os, Barcode x)
   {
-    os << x.vertex() << "|" << x.primary() << "|" << x.generation() << "|"
-       << x.secondary() << "|" << x.process();
+    os << x.vertexPrimary() << "|" << x.vertexSecondary() << "|" << x.particle()
+       << "|" << x.parentParticle() << "|" << x.process();
     return os;
   }
 };
