c1b9a222f18d1b52eacaf3de2180cf318ee95d2e
Moritz Kiehn <msmk@cern.ch>
2020-02-21 18:06:18 +0100

    Io/Root/Particles: unify branch names and simplify config

--- Io/Root/src/RootParticleWriter.cpp
+++ Io/Root/src/RootParticleWriter.cpp
@@ -11,63 +11,55 @@
 #include <ios>
 #include <stdexcept>
 
-#include <Acts/Utilities/Helpers.hpp>
-#include <Acts/Utilities/Units.hpp>
 #include <TFile.h>
 #include <TTree.h>
 
-using Acts::VectorHelpers::eta;
-using Acts::VectorHelpers::perp;
-using Acts::VectorHelpers::phi;
+#include "Acts/Utilities/Helpers.hpp"
+#include "Acts/Utilities/Units.hpp"
 
 FW::RootParticleWriter::RootParticleWriter(
     const FW::RootParticleWriter::Config& cfg,
-    Acts::Logging::Level                  level)
-  : WriterT(cfg.collection, "RootParticleWriter", level)
-  , m_cfg(cfg)
-  , m_outputFile(cfg.rootFile)
+    Acts::Logging::Level                  lvl)
+  : WriterT(cfg.inputParticles, "RootParticleWriter", lvl), m_cfg(cfg)
 {
-  // An input collection name and tree name must be specified
-  if (m_cfg.collection.empty()) {
-    throw std::invalid_argument("Missing input collection");
-  } else if (m_cfg.treeName.empty()) {
+  // inputParticles is already checked by base constructor
+  if (m_cfg.filePath.empty()) {
+    throw std::invalid_argument("Missing file path");
+  }
+  if (m_cfg.treeName.empty()) {
     throw std::invalid_argument("Missing tree name");
   }
 
-  // Setup ROOT I/O
+  // open root file and create the tree
+  m_outputFile = TFile::Open(m_cfg.filePath.c_str(), m_cfg.fileMode.c_str());
   if (m_outputFile == nullptr) {
-    m_outputFile = TFile::Open(m_cfg.filePath.c_str(), m_cfg.fileMode.c_str());
-    if (m_outputFile == nullptr) {
-      throw std::ios_base::failure("Could not open '" + m_cfg.filePath);
-    }
+    throw std::ios_base::failure("Could not open '" + m_cfg.filePath + "'");
   }
   m_outputFile->cd();
   m_outputTree = new TTree(m_cfg.treeName.c_str(), m_cfg.treeName.c_str());
-  if (m_outputTree == nullptr)
-    throw std::bad_alloc();
-  else {
-    // I/O parameters
-    m_outputTree->Branch("event_nr", &m_eventNr);
-    m_outputTree->Branch("eta", &m_eta);
-    m_outputTree->Branch("phi", &m_phi);
-    m_outputTree->Branch("vx", &m_vx);
-    m_outputTree->Branch("vy", &m_vy);
-    m_outputTree->Branch("vz", &m_vz);
-    m_outputTree->Branch("vt", &m_vt);
-    m_outputTree->Branch("px", &m_px);
-    m_outputTree->Branch("py", &m_py);
-    m_outputTree->Branch("pz", &m_pz);
-    m_outputTree->Branch("pt", &m_pT);
-    m_outputTree->Branch("charge", &m_charge);
-    m_outputTree->Branch("mass", &m_mass);
-    m_outputTree->Branch("pdg", &m_pdgCode);
-    m_outputTree->Branch("barcode", &m_barcode, "barcode/l");
-    m_outputTree->Branch("vertex_primary", &m_vertexPrimary);
-    m_outputTree->Branch("vertex_secondary", &m_vertexSecondary);
-    m_outputTree->Branch("particle", &m_particle);
-    m_outputTree->Branch("parent_particle", &m_parentParticle);
-    m_outputTree->Branch("process", &m_process);
-  }
+  if (m_outputTree == nullptr) { throw std::bad_alloc(); }
+
+  // setup the branches
+  m_outputTree->Branch("event_id", &m_eventId);
+  m_outputTree->Branch("particle_id", &m_particleId, "particle_id/l");
+  m_outputTree->Branch("particle_type", &m_particleType);
+  m_outputTree->Branch("process", &m_process);
+  m_outputTree->Branch("vx", &m_vx);
+  m_outputTree->Branch("vy", &m_vy);
+  m_outputTree->Branch("vz", &m_vz);
+  m_outputTree->Branch("vt", &m_vt);
+  m_outputTree->Branch("px", &m_px);
+  m_outputTree->Branch("py", &m_py);
+  m_outputTree->Branch("pz", &m_pz);
+  m_outputTree->Branch("m", &m_m);
+  m_outputTree->Branch("q", &m_q);
+  m_outputTree->Branch("eta", &m_eta);
+  m_outputTree->Branch("phi", &m_phi);
+  m_outputTree->Branch("pt", &m_pt);
+  m_outputTree->Branch("vertex_primary", &m_vertexPrimary);
+  m_outputTree->Branch("vertex_secondary", &m_vertexSecondary);
+  m_outputTree->Branch("particle", &m_particle);
+  m_outputTree->Branch("parent_particle", &m_parentParticle);
 }
 
 FW::RootParticleWriter::~RootParticleWriter()
@@ -88,45 +80,43 @@ FW::RootParticleWriter::endRun()
 }
 
 FW::ProcessCode
-FW::RootParticleWriter::writeT(const AlgorithmContext&             context,
-                               const std::vector<Data::SimVertex>& vertices)
+FW::RootParticleWriter::writeT(const AlgorithmContext& ctx,
+                               const SimParticles&     particles)
 {
+  if (not m_outputFile) {
+    ACTS_ERROR("Missing output file");
+    return ProcessCode::ABORT;
+  }
 
-  if (m_outputFile == nullptr) return ProcessCode::SUCCESS;
-
-  // Exclusive access to the tree while writing
+  // ensure exclusive access to tree/file while writing
   std::lock_guard<std::mutex> lock(m_writeMutex);
 
-  // Get the event number
-  m_eventNr = context.eventNumber;
-
-  // loop over the process vertices
-  for (auto& vertex : vertices) {
-    for (auto& particle : vertex.outgoing) {
-      // collect the information
-      m_vx      = particle.position().x();
-      m_vy      = particle.position().y();
-      m_vz      = particle.position().z();
-      m_vt      = particle.time() / Acts::UnitConstants::ns;
-      m_eta     = eta(particle.momentum());
-      m_phi     = phi(particle.momentum());
-      m_px      = particle.momentum().x();
-      m_py      = particle.momentum().y();
-      m_pz      = particle.momentum().z();
-      m_pT      = perp(particle.momentum());
-      m_charge  = particle.q();
-      m_mass    = particle.m();
-      m_pdgCode = particle.pdg();
-      // store encoded barcode
-      m_barcode = particle.barcode().value();
-      // store decoded barcode components
-      m_vertexPrimary   = particle.barcode().vertexPrimary();
-      m_vertexSecondary = particle.barcode().vertexSecondary();
-      m_particle        = particle.barcode().particle();
-      m_parentParticle  = particle.barcode().parentParticle();
-      m_process         = particle.barcode().process();
-      m_outputTree->Fill();
-    }
+  m_eventId = ctx.eventNumber;
+  for (const auto& particle : particles) {
+    m_particleId   = particle.barcode().value();
+    m_particleType = particle.pdg();
+    m_process      = particle.barcode().process();
+    // collect the information
+    m_vx = particle.position().x() / Acts::UnitConstants::mm;
+    m_vy = particle.position().y() / Acts::UnitConstants::mm;
+    m_vz = particle.position().z() / Acts::UnitConstants::mm;
+    m_vt = particle.time() / Acts::UnitConstants::ns;
+    m_px = particle.momentum().x() / Acts::UnitConstants::GeV;
+    m_py = particle.momentum().y() / Acts::UnitConstants::GeV;
+    m_pz = particle.momentum().z() / Acts::UnitConstants::GeV;
+    m_m  = particle.m() / Acts::UnitConstants::GeV;
+    m_q  = particle.q() / Acts::UnitConstants::e;
+    // derived kinematic quantities
+    m_eta = Acts::VectorHelpers::eta(particle.momentum());
+    m_phi = Acts::VectorHelpers::phi(particle.momentum());
+    m_pt  = Acts::VectorHelpers::perp(particle.momentum())
+        / Acts::UnitConstants::GeV;
+    // decoded barcode components
+    m_vertexPrimary   = particle.barcode().vertexPrimary();
+    m_vertexSecondary = particle.barcode().vertexSecondary();
+    m_particle        = particle.barcode().particle();
+    m_parentParticle  = particle.barcode().parentParticle();
+    m_outputTree->Fill();
   }
 
   return ProcessCode::SUCCESS;
