3034e91843818d4ce65f6fe4b4bd6c91d8fa996d
Moritz Kiehn <msmk@cern.ch>
2020-03-02 10:58:26 +0100

    Io/Csv: document all output columns with units

--- Io/Csv/src/TrackMlData.hpp
+++ Io/Csv/src/TrackMlData.hpp
@@ -19,16 +19,21 @@ namespace FW {
 
 struct ParticleData
 {
+  /// Event-unique particle identifier a.k.a barcode.
   uint64_t particle_id;
-  int32_t  particle_type;
-  /// defaulted, process type is not available in the TrackML datasets
+  /// Particle type number a.k.a. PDG particle number.
+  int32_t particle_type;
+  /// Production process type. Not available in the TrackML datasets.
   uint32_t process = 0u;
-  float    vx, vy, vz;
-  // defaulted, time is not available in the TrackML datasets
+  /// Production position components in mm.
+  float vx, vy, vz;
+  // Production time in ns. Not available in the TrackML datasets.
   float vt = 0.0f;
+  /// Momentum components in GeV.
   float px, py, pz;
-  // defaulted, mass is not available in the TrackML datasets
+  /// Mass in GeV. Not available in the TrackML datasets
   float m = 0.0f;
+  /// Charge in e.
   float q;
 
   DFE_NAMEDTUPLE(ParticleData,
@@ -48,22 +53,30 @@ struct ParticleData
 
 struct TruthHitData
 {
+  /// Event-unique hit identifier. As defined for the simulated hit below and
+  /// used to link back to it; same value can appear multiple times here due to
+  /// shared hits in dense environments.
   uint64_t hit_id;
+  /// Event-unique particle identifier of the generating particle.
   uint64_t particle_id;
-  // defaulted, geometry identifier is not available in the TrackML datasets
+  /// Hit surface identifier. Not available in the TrackML datasets.
   uint64_t geometry_id = 0u;
-  float    tx, ty, tz;
-  // defaulted, time is not available in the TrackML datasets
+  /// True global hit position components in mm.
+  float tx, ty, tz;
+  // True global hit time in ns. Not available in the TrackML datasets.
   float tt = 0.0f;
+  /// True particle momentum in GeV before interaction.
   float tpx, tpy, tpz;
-  // defaulted, particle energy is not available in the TrackML datasets
+  /// True particle energy in GeV before interaction.
+  /// Not available in the TrackML datasets.
   float te = 0.0f;
-  /// defaulted, four-momentum change is not available in the TrackML datasets
+  /// True four-momentum change in GeV due to interaction.
+  /// Not available in the TrackML datasets.
   float deltapx = 0.0f;
   float deltapy = 0.0f;
   float deltapz = 0.0f;
   float deltae  = 0.0f;
-  // defaulted, hit index is not available in the TrackML datasets
+  // Hit index along the trajectory. Not available in the TrackML datasets.
   int32_t index = -1;
 
   DFE_NAMEDTUPLE(TruthHitData,
@@ -87,12 +100,15 @@ struct TruthHitData
 
 struct HitData
 {
+  /// Event-unique hit identifier. Each value can appear at most once.
   uint64_t hit_id;
-  // defaulted, geometry identifier is not available in the TrackML datasets
+  /// Hit surface identifier. Not available in the TrackML datasets.
   uint64_t geometry_id = 0u;
+  /// Partially decoded hit surface identifier components.
   uint32_t volume_id, layer_id, module_id;
-  float    x, y, z;
-  // defaulted, time is not available in the TrackML datasets
+  /// Global hit position components in mm.
+  float x, y, z;
+  /// Global hit time in ns. Not available in the TrackML datasets.
   float t = 0.0f;
 
   DFE_NAMEDTUPLE(HitData,
@@ -109,12 +125,17 @@ struct HitData
 
 struct CellData
 {
+  /// Event-unique hit identifier. As defined for the simulated hit above and
+  /// used to link back to it; same value can appear multiple times for clusters
+  /// with more than one active cell.
   uint64_t hit_id;
-  // These should have been named channel{0,1} but we cannot change it now
-  // to avoid breaking backward compatibility.
+  /// Digital cell address/ channel identifier. These should have been named
+  /// channel{0,1} but we cannot change it now to avoid breaking backward
+  /// compatibility.
   int32_t ch0, ch1;
-  // defaulted, timestamp is not available in the TrackML datasets
+  /// Digital cell timestamp. Not available in the TrackML datasets.
   int32_t timestamp = 0;
+  /// (Digital) measured cell value, e.g. amplitude or time-over-threshold.
   int32_t value;
 
   DFE_NAMEDTUPLE(CellData, hit_id, ch0, ch1, timestamp, value);
@@ -122,13 +143,17 @@ struct CellData
 
 struct SurfaceData
 {
+  /// Surface identifier. Not available in the TrackML datasets.
   uint64_t geometry_id;
+  /// Partially decoded surface identifier components.
   uint32_t volume_id, layer_id, module_id;
-  float    cx, cy, cz;
-  float    rot_xu, rot_xv, rot_xw;
-  float    rot_yu, rot_yv, rot_yw;
-  float    rot_zu, rot_zv, rot_zw;
-  // Limits and pitches are not always available and need invalid defaults.
+  /// Center position components in mm.
+  float cx, cy, cz;
+  /// Rotation matrix components.
+  float rot_xu, rot_xv, rot_xw;
+  float rot_yu, rot_yv, rot_yw;
+  float rot_zu, rot_zv, rot_zw;
+  /// Limits and pitches in mm. Not always available.
   float module_t     = -1;
   float module_minhu = -1;
   float module_maxhu = -1;
