0a89ef723dc9cf34e2d609c131c97f05df3e27f4
Moritz Kiehn <msmk@cern.ch>
2020-03-05 18:09:25 +0100

    Pythia8: use consistent option naming

--- Plugins/Pythia8/ACTFW/Options/Pythia8Options.cpp
+++ Plugins/Pythia8/ACTFW/Options/Pythia8Options.cpp
@@ -13,38 +13,41 @@
 #include "ACTFW/Generators/MultiplicityGenerators.hpp"
 #include "ACTFW/Generators/Pythia8ProcessGenerator.hpp"
 #include "ACTFW/Generators/VertexGenerators.hpp"
+#include "Acts/Utilities/PdgParticle.hpp"
+#include "Acts/Utilities/Units.hpp"
 
 void
 FW::Options::addPythia8Options(boost::program_options::options_description& opt)
 {
   using namespace boost::program_options;
 
-  opt.add_options()("evg-cmsEnergy",
+  opt.add_options()("evg-cms-energy",
                     value<double>()->default_value(14000.),
-                    "CMS value of the beam in [GeV].")(
-      "evg-pdgBeam0",
-      value<int>()->default_value(2212),
-      "PDG number of beam 0 particles.")("evg-pdgBeam1",
-                                         value<int>()->default_value(2212),
-                                         "PDG number of beam 1 particles.")(
-      "evg-hsProcess",
+                    "Center-of-mass energy collision in GeV")(
+      "evg-beam0",
+      value<int32_t>()->default_value(Acts::PdgParticle::eProton),
+      "PDG number of the first beam particle")(
+      "evg-beam1",
+      value<int32_t>()->default_value(Acts::PdgParticle::eProton),
+      "PDG number of the second beam particle")(
+      "evg-hard-process",
       value<std::string>()->default_value("HardQCD:all = on"),
-      "The process string for the hard scatter event.")(
-      "evg-puProcess",
+      "Pythia8 process string for the hard scatter")(
+      "evg-pileup-process",
       value<std::string>()->default_value("SoftQCD:all = on"),
-      "The process string for the pile-up events.")(
+      "Pythi8 process string for the pile-up")(
       "evg-pileup",
-      value<int>()->default_value(200),
-      "Number of instantaneous pile-up events.")(
+      value<size_t>()->default_value(200),
+      "Number of instantaneous pile-up events")(
       "evg-vertex-xy-std",
       value<double>()->default_value(0.015),
-      "Transverse vertex standard deviation in [mm].")(
+      "Transverse vertex standard deviation in mm")(
       "evg-vertex-z-std",
       value<double>()->default_value(55.5),
-      "Longitudinal vertex standard deviation in [mm].")(
+      "Longitudinal vertex standard deviation in mm")(
       "evg-vertex-t-std",
       value<double>()->default_value(0.08),
-      "Timestamp vertex standard deviation in [ns].")(
+      "Temporal vertex standard deviation in ns")(
       "evg-shuffle", bool_switch(), "Randomnly shuffle the vertex order.");
 }
 
@@ -52,35 +55,36 @@ FW::EventGenerator::Config
 FW::Options::readPythia8Options(const boost::program_options::variables_map& vm,
                                 Acts::Logging::Level lvl)
 {
-  Pythia8Generator::Config hardCfg;
-  hardCfg.pdgBeam0  = vm["evg-pdgBeam0"].template as<int>();
-  hardCfg.pdgBeam1  = vm["evg-pdgBeam1"].template as<int>();
-  hardCfg.cmsEnergy = vm["evg-cmsEnergy"].template as<double>();
-  hardCfg.settings  = {vm["evg-hsProcess"].template as<std::string>()};
-  Pythia8Generator::Config pileupCfg;
-  pileupCfg.pdgBeam0  = vm["evg-pdgBeam0"].template as<int>();
-  pileupCfg.pdgBeam1  = vm["evg-pdgBeam1"].template as<int>();
-  pileupCfg.cmsEnergy = vm["evg-cmsEnergy"].template as<double>();
-  pileupCfg.settings  = {vm["evg-puProcess"].template as<std::string>()};
+  Pythia8Generator::Config hard;
+  hard.pdgBeam0 = static_cast<Acts::PdgParticle>(vm["evg-beam0"].as<int32_t>());
+  hard.pdgBeam1 = static_cast<Acts::PdgParticle>(vm["evg-beam1"].as<int32_t>());
+  hard.cmsEnergy = vm["evg-cms-energy"].as<double>();
+  hard.settings  = {vm["evg-hard-process"].as<std::string>()};
 
+  Pythia8Generator::Config pileup;
+  pileup.pdgBeam0
+      = static_cast<Acts::PdgParticle>(vm["evg-beam0"].as<int32_t>());
+  pileup.pdgBeam1
+      = static_cast<Acts::PdgParticle>(vm["evg-beam1"].as<int32_t>());
+  pileup.cmsEnergy = vm["evg-cms-energy"].as<double>();
+  pileup.settings  = {vm["evg-pileup-process"].as<std::string>()};
+
+  auto mu = vm["evg-pileup"].as<size_t>();
   auto vtxStdXY
-      = vm["evg-vertex-xy-std"].template as<double>() * Acts::UnitConstants::mm;
-  auto vtxStdZ
-      = vm["evg-vertex-z-std"].template as<double>() * Acts::UnitConstants::mm;
-  auto vtxStdT
-      = vm["evg-vertex-t-std"].template as<double>() * Acts::UnitConstants::ns;
-  auto mu = static_cast<size_t>(vm["evg-pileup"].template as<int>());
+      = vm["evg-vertex-xy-std"].as<double>() * Acts::UnitConstants::mm;
+  auto vtxStdZ = vm["evg-vertex-z-std"].as<double>() * Acts::UnitConstants::mm;
+  auto vtxStdT = vm["evg-vertex-t-std"].as<double>() * Acts::UnitConstants::ns;
 
   EventGenerator::Config cfg;
   cfg.generators = {
       {FixedMultiplicityGenerator{1},
        GaussianVertexGenerator{vtxStdXY, vtxStdXY, vtxStdZ, vtxStdT},
-       Pythia8Generator::makeFunction(hardCfg, lvl)},
+       Pythia8Generator::makeFunction(hard, lvl)},
       {PoissonMultiplicityGenerator{mu},
        GaussianVertexGenerator{vtxStdXY, vtxStdXY, vtxStdZ, vtxStdT},
-       Pythia8Generator::makeFunction(pileupCfg, lvl)},
+       Pythia8Generator::makeFunction(hard, lvl)},
   };
-  cfg.shuffle = vm["evg-shuffle"].template as<bool>();
+  cfg.shuffle = vm["evg-shuffle"].as<bool>();
 
   return cfg;
 }
