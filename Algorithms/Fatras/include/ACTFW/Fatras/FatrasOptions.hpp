3e1f9c86fc000b0cc8707568f03dd217ab3e9060
Moritz Kiehn <msmk@cern.ch>
2020-03-06 13:55:11 +0100

    Fatras: rewrite algorithm based on updated edm/interface

--- Algorithms/Fatras/include/ACTFW/Fatras/FatrasOptions.hpp
+++ Algorithms/Fatras/include/ACTFW/Fatras/FatrasOptions.hpp
@@ -14,11 +14,10 @@
 
 #include "ACTFW/Fatras/FatrasAlgorithm.hpp"
 #include "ACTFW/Utilities/OptionsFwd.hpp"
+#include "Acts/Utilities/Logger.hpp"
 #include "Acts/Utilities/Units.hpp"
-#include "ActsFatras/Kernel/SelectorList.hpp"
-#include "ActsFatras/Selectors/ChargeSelectors.hpp"
-#include "ActsFatras/Selectors/KinematicCasts.hpp"
-#include "ActsFatras/Selectors/SelectorHelpers.hpp"
+#include "ActsFatras/Kernel/Process.hpp"
+#include "ActsFatras/Physics/StandardPhysicsLists.hpp"
 
 namespace FW {
 namespace Options {
@@ -36,27 +35,64 @@ namespace Options {
   /// @param simulator    the simulation kernel
   template <typename simulator_t>
   typename FatrasAlgorithm<simulator_t>::Config
-  readFatrasConfig(const Variables& vars, simulator_t&& simulator)
+  readFatrasConfig(const Variables& variables, simulator_t&& simulator)
   {
     using namespace Acts::UnitLiterals;
-    using Config    = typename FatrasAlgorithm<simulator_t>::Config;
-    using PtMin     = ActsFatras::Min<ActsFatras::Casts::pT>;
-    using EMin      = ActsFatras::Min<ActsFatras::Casts::E>;
-    using AbsEtaMax = ActsFatras::Max<ActsFatras::Casts::absEta>;
+    using Config = typename FatrasAlgorithm<simulator_t>::Config;
+    using PMin   = ActsFatras::Min<ActsFatras::Casts::P>;
+
+    using Acts::Logger;
+    ACTS_LOCAL_LOGGER(
+        Acts::getDefaultLogger("FatrasOptions", Acts::Logging::INFO))
 
     Config cfg(std::forward<simulator_t>(simulator));
 
-    // set particle cuts
-    cfg.simulator.chargedSelector.template get<PtMin>().valMin     = 100_MeV;
-    cfg.simulator.chargedSelector.template get<AbsEtaMax>().valMax = 5.;
-    cfg.simulator.neutralSelector.template get<EMin>().valMin      = 100_MeV;
-    cfg.simulator.neutralSelector.template get<AbsEtaMax>().valMax = 5.;
+    // simulation particle cuts both for input and physics lists output
+    const auto pmin = variables["fatras-pmin-gev"].as<double>() * 1_GeV;
+    cfg.simulator.selectCharged.template get<PMin>().valMin = pmin;
+    cfg.simulator.selectNeutral.template get<PMin>().valMin = pmin;
+    cfg.simulator.charged.physics
+        = ActsFatras::makeChargedElectroMagneticPhysicsList(pmin);
 
-    // set the input collections
-    cfg.simulatedHitCollection
-        = vars["fatras-sim-hits"].template as<std::string>();
-    cfg.simulatedEventCollection
-        = vars["fatras-sim-particles"].template as<std::string>();
+    // all physics process are enabled by default
+    if (not variables["fatras-em-scattering"].as<bool>()) {
+      cfg.simulator.charged.physics
+          .template disable<ActsFatras::detail::StandardScattering>();
+    }
+    if (not variables["fatras-em-ionisation"].as<bool>()) {
+      cfg.simulator.charged.physics
+          .template disable<ActsFatras::detail::StandardBetheBloch>();
+    }
+    if (not variables["fatras-em-radiation"].as<bool>()) {
+      cfg.simulator.charged.physics
+          .template disable<ActsFatras::detail::StandardBetheHeitler>();
+    }
+
+    // select hit surfaces for charged particles
+    const std::string hits = variables["fatras-hits"].as<std::string>();
+    if (hits == "sensitive") {
+      ACTS_DEBUG("Configure hits on sensitive surfaces");
+      cfg.simulator.charged.selectHitSurface.sensitive = true;
+      cfg.simulator.charged.selectHitSurface.material  = false;
+      cfg.simulator.charged.selectHitSurface.passive   = false;
+    } else if (hits == "material") {
+      ACTS_DEBUG("Configure hits on material surfaces");
+      cfg.simulator.charged.selectHitSurface.sensitive = false;
+      cfg.simulator.charged.selectHitSurface.material  = true;
+      cfg.simulator.charged.selectHitSurface.passive   = false;
+    } else if (hits == "all") {
+      ACTS_DEBUG("Configure hits on all surfaces");
+      cfg.simulator.charged.selectHitSurface.sensitive = false;
+      cfg.simulator.charged.selectHitSurface.material  = false;
+      // this includes sensitive and material surfaces
+      cfg.simulator.charged.selectHitSurface.passive = true;
+    } else {
+      ACTS_WARNING("Invalid hits configuration '" << hits << "'");
+      // none or unknown type -> record nothing
+      cfg.simulator.charged.selectHitSurface.sensitive = false;
+      cfg.simulator.charged.selectHitSurface.material  = false;
+      cfg.simulator.charged.selectHitSurface.passive   = false;
+    }
 
     return cfg;
   }
